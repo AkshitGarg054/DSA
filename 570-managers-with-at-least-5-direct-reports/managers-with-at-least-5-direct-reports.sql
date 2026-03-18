ad3cb903971074e1cb8e0a6d84cd8a2ec47cc0ef
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-18 16:17:06 +0530

    Time: 347 ms (44.19%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,12 @@
-# using JOIN
-# The "Self-Join" Concept: In this table, managers are also employees. To get the name of the manager who has 5 reports, you have to look at the table twice: once to count the reports and once to find the name associated with that managerId.
-# Step A: Who are the IDs that appear at least 5 times in the managerId column? 
+# using SUBQUERY
+# Step A: write a subquery to extract the IDs that appear at least 5 times in the managerId column? 
 # Step B: What are the names of the employees whose id matches those IDs?
 
-SELECT e1.name
-FROM Employee e1 JOIN Employee e2
-ON e1.id = e2.managerId
-GROUP BY e2.managerId
-HAVING COUNT(e2.managerId) >= 5;
+SELECT name 
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(managerId) >= 5
+);
