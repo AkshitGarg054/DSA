51ef0a380ab963e5aed2c54a937d8bd5c0cd4aa1
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-18 23:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (34.36%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // always remember that : to check whether a string y is present in string x as a substring or not, we follow this template
+    // always remember that : to check whether a string y is present in string x as a subsequence or not, we follow this template
 
     int appendCharacters(string s, string t) {
         int i = 0, j = 0;
