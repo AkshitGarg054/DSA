1639798d244e5648e4631533d850e904f7fc9e6c
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-18 16:07:24 +0530

    Time: 335 ms (60.49%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,8 @@
+# using JOIN
+# The "Self-Join" Concept: In this table, managers are also employees. To get the name of the manager who has 5 reports, you have to look at the table twice: once to count the reports and once to find the name associated with that managerId.
+# Step A: Who are the IDs that appear at least 5 times in the managerId column? 
+# Step B: What are the names of the employees whose id matches those IDs?
+
 SELECT e1.name
 FROM Employee e1 JOIN Employee e2
 ON e1.id = e2.managerId
