1b3dbbd822f92dd7ef96a89d1882632c13cba3e6
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-19 22:35:24 +0530

    Time: 3 ms (17.62%) | Memory: 45.2 MB (60.67%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    // in normal kadane question, we find the normal maximum subarray sum
+    // but here, absolute sum ka maximum nikalna haiii
+    // mtlb maximum negative sum jo ayega, vo bhi hm consider kr skte hn, uska absolute leke.
+    // isliye, i thought of ki, saare elements ka sign change kr deta hu, to maximum negative sum will become positive
+    // and fir last me, normal max sum, and negative max sum wale ko compare krke, max return kr dunga
+
     int maxAbsoluteSum(vector<int>& nums) {
         int n = nums.size();
         int ans1 = INT_MIN, sum = 0;
