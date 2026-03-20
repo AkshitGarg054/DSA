5889b11d9e028a22d0fddc4b24643e2335a186c0
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-20 17:45:02 +0530

    Time: 252 ms (34.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,16 @@
+SELECT s.machine_id, ROUND((e.end_time - s.start_time) / e.total_processes, 3) AS processing_time
+FROM (
+    SELECT machine_id, SUM(timestamp) as end_time, COUNT(process_id) AS total_processes
+    FROM Activity
+    WHERE activity_type = 'end'
+    GROUP BY machine_id
+) e
+JOIN (
+    SELECT machine_id, SUM(timestamp) as start_time
+    FROM Activity
+    WHERE activity_type = 'start'
+    GROUP BY machine_id
+) s
+ON s.machine_id = e.machine_id;
+
+
