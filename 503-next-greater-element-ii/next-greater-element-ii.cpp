d8cd0af1ce7145ae9bab28362fac9d4c50778d65
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-20 16:54:39 +0530

    Time: 12 ms (13.40%) | Memory: 28.6 MB (20.36%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -3,20 +3,22 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> nge(2*n, -1);
+        // This vector is not used in this code, we can use it also.
+        // If we use it, then there is no need to do (i % n)
+        // vector<int> vec(nums.begin(), nums.end());
+        // for(int i = 0; i < n; i++) vec.push_back(nums[i]);
+
+        vector<int> nge(n, -1);
         stack<int> st; // store indices
 
         for(int i = 0; i < 2*n; i++) {
             while(!st.empty() && nums[i % n] > nums[st.top() % n]) {
-                nge[st.top()] = nums[i % n];
+                nge[st.top() % n] = nums[i % n];
                 st.pop();
             }
             st.push(i);
         }
 
-        vector<int> ans(n);
-        for(int i = 0; i < n; i++) ans[i] = nge[i];
-
-        return ans;
+        return nge;
     }
 };
