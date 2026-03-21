833dfd1b08192d8feac5f4f0e0beae5856ec6d64
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-21 22:43:45 +0530

    Time: 323 ms (97.45%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,3 +1,6 @@
+## Whenever consecutive rows ki baat ho, then think of ROW_NUMBER and then take diff with it
+## consecutive rows have constant difference
+
 SELECT id, visit_date, people
 FROM (
     SELECT *, id - ROW_NUMBER() OVER (ORDER BY id ASC) as diff
@@ -10,8 +13,7 @@ WHERE diff in (
         SELECT id - ROW_NUMBER() OVER (ORDER BY id ASC) as diff
         FROM Stadium
         WHERE people >= 100
-    ) t2
+    ) t2                    
     GROUP BY diff 
     HAVING COUNT(diff) >= 3 
 )
- 
