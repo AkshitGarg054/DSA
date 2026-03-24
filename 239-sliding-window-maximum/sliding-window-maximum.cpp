03c1698707109175e39cc61afb056a49243d3af3
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-24 23:42:59 +0530

    Time: 1226 ms (5.14%) | Memory: 136.9 MB (94.61%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> ans;
+        int maxi = INT_MIN;
+        int maxi_freq = 0;
+
+        for(int i = 0; i < k; i++) {
+            if(nums[i] > maxi) {
+                maxi = nums[i];
+                maxi_freq = 1;
+            }
+            else if(nums[i] == maxi) maxi_freq++;
+        }
+
+        ans.push_back(maxi);
+
+        int l = 0, r = k;
+
+        while(r < n) {
+            // extend window
+            if(nums[r] > maxi) {
+                maxi = nums[r];
+                maxi_freq = 1;
+                l++; // to always maintain k size
+            }
+            else if(nums[r] == maxi) {
+                if(nums[l] != maxi) maxi_freq++;
+                // maxi_freq++;
+                l++;
+            }
+            else { // nums[r] < maxi
+                if(nums[l] == maxi && maxi_freq > 1) {
+                    maxi_freq--;
+                    l++;
+                }
+                else if(nums[l] == maxi && maxi_freq == 1) {
+                    l++;
+
+                    maxi = INT_MIN;
+                    maxi_freq = 0;
+
+                    for(int i = l; i <= r; i++) {
+                        if(nums[i] > maxi) {
+                            maxi = nums[i];
+                            maxi_freq = 1;
+                        }
+                        else if(nums[i] == maxi) maxi_freq++;
+                    }
+                }
+                else if(nums[l] != maxi) l++;
+            }
+
+            ans.push_back(maxi);
+            r++;
+        }
+
+        return ans;
+    }
+};
