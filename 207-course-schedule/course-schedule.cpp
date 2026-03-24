211990bb194b106c63e3d0711e01d5275e9da0b1
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-24 16:50:22 +0530

    Time: 7 ms (47.07%) | Memory: 20.4 MB (15.87%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    // using DFS, instead of topo sort
+    // If cycle founds in DFS, then finishing all courses is not possible. Example : [[1, 0], [1, 2], [0, 1]], n = 3
+    // To detect cycle, we need three visited states
+    // 0 -> unvisited 
+    // 1 -> visiting (currently in recursion stack)
+    // 2 -> visited
+
+    // this function is made to detect cycle
+    bool dfs(int u, vector<vector<int>> &list, vector<int> &vis) { 
+        vis[u] = 1; // visiting
+        
+        for(auto v : list[u]) {
+            if(vis[v] == 1) return true; // cycle found
+            if(vis[v] == 0) {
+                if(dfs(v, list, vis) == true) return true;
+            }
+        }
+
+        vis[u] = 2; // visited
+        return false;
+    }
+  
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> list(n);
+        vector<int> inDegree(n, 0);
+        
+        for(auto edge : prerequisites) {
+            int u = edge[1], v = edge[0];
+            list[u].push_back(v);
+            inDegree[v]++;
+        }
+        
+        vector<int> vis(n, 0);
+        
+        for(int i = 0; i < n; i++) {
+            if(vis[i] == 0) {
+                if(dfs(i, list, vis)) return false;
+            }
+        }
+        
+        return true;
+    }
+};
