1979072e5aa9af1dc15760607d6522886d2624f7
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-26 00:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (21.96%) - LeetSync

---
+++ 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // We only have two types of mismatch : xy mismatch or yx mismatch
+    // if xy, xy OR yx, yx mismatch are present --> can be fixed in 1 swap
+    // but, xy, yx mismatch requires --> 2 swaps
+
+    // There are two questions --
+    // Whether it is possible to make the strings equal?
+    // count of mismatches (xy + yx) should be even -> i.e either both odd or both even
+    // How many swaps are needed?
+    // if both xy and yx are even --> (xy + yx)/2 swaps 
+    // if both xy and yx are odd --> xy/2 + yx/2 + 2 swaps
+
+    int minimumSwap(string s1, string s2) {
+        int n = s1.size();
+        int xy = 0, yx = 0;
+    
+        for(int i = 0; i < n; i++) {
+            if(s1[i] == 'x' && s2[i] == 'y') xy++;
+            else if(s1[i] == 'y' && s2[i] == 'x') yx++;
+        }
+        
+        if((xy + yx) % 2 != 0) return -1;
+
+        if(xy % 2 == 0 && yx % 2 == 0) return (xy + yx) / 2;
+        return xy/2 + yx/2 + 2;
+    }
+};
