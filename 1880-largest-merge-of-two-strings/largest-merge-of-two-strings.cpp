cfef5ce126390012dbcb8c79a115821567906e21
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-26 16:30:34 +0530

    Time: 75 ms (56.53%) | Memory: 166.9 MB (54.55%) - LeetSync

---
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string largestMerge(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        string merge = "";
+
+        int i = 0, j = 0;
+        while(i < n && j < m) {
+            if(word1[i] > word2[j]) merge += word1[i++];
+            else if(word1[i] < word2[j]) merge += word2[j++];
+            else {
+                if(word1.substr(i) > word2.substr(j)) merge += word1[i++];
+                else merge += word2[j++];
+            }
+        }
+
+        while(i < n) merge += word1[i++];
+        while(j < m) merge += word2[j++];
+
+        return merge;
+    }
+};
