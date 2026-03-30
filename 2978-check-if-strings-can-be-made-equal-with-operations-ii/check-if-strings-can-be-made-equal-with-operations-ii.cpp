53238b5b7525916a68c96e8af755b12e823a456a
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-30 09:49:49 +0530

    Time: 11 ms (47.70%) | Memory: 19.8 MB (68.97%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    // j - i = even means,
+    // a character at an even index can only go to another even index
+    // a character at an odd index can only go to another odd index
+    // So the string is basically split into 2 independent groups:
+    // even-index characters, odd-index characters
+    // and inside each group, you can permute freely.
+    // So, Even positions of both strings should contain the same multiset of characters
+    // and, Odd positions of both strings should contain the same multiset of characters
+
     bool checkStrings(string s1, string s2) {
         int n = s1.size();
         vector<int> even1(26, 0), odd1(26, 0);
