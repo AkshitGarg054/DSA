c4923162862c91214d86e388eeacefab06055936
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-03-30 20:33:05 +0530

    Time: 8 ms (23.56%) | Memory: 18.3 MB (81.68%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        auto cmp = [&](ListNode *a, ListNode* b) {
+            return a -> val > b -> val; // a niche hoga, b upar hoga.
+        };
+
+        priority_queue<ListNode*, vector<ListNode*>,  decltype(cmp)> pq(cmp);
+
+        for(auto head : lists) {
+            if(head != NULL) pq.push(head);
+        }
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while(!pq.empty()) {
+            ListNode* curr = pq.top();
+            pq.pop();
+
+            temp -> next = curr;
+            temp = temp -> next;
+
+            if(curr -> next) pq.push(curr -> next);
+        }
+
+        return dummy -> next;
+    }
+};
