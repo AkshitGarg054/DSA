ec152b01743d383d59becacc170f757ee8bdf3fe
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-03 22:47:11 +0530

    Time: 737 ms (25.94%) | Memory: 12.2 MB (44.71%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int solve(int index, int sum, int k, int target, vector<int> &nums, vector<int> &vis) {
         if(k == 1) return true; // if only 1 subset left, remaining elements must form it
-        if(sum == target) {
-            return solve(0, 0, k-1, target, nums, vis);
+        if(sum == target) return solve(0, 0, k-1, target, nums, vis);
+        if(index == nums.size()) return false; // we reached end, and no valid subset formed 
+
+        // take
+        if(!vis[index] && sum + nums[index] <= target) {
+            vis[index] = 1;
+            if(solve(index + 1, sum + nums[index], k, target, nums, vis) == true) return true;
+            vis[index] = 0;
         }
 
-        for(int i = index; i < nums.size(); i++) {
-            if(vis[i]) continue;
-            if(sum + nums[i] > target) continue;
-
-            vis[i] = 1;
-            if(solve(i + 1, sum + nums[i], k, target, nums, vis) == true) return true;
-            vis[i] = 0; // backtrack
-        }
-
-        return false;
+        // not take
+        return solve(index + 1, sum, k, target, nums, vis);
     }
 
     bool canPartitionKSubsets(vector<int>& nums, int k) {
