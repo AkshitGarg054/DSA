c634f708138bd49011c21c472f9f2a90380daaa6
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-04 14:06:21 +0530

    Time: 15 ms (19.56%) | Memory: 38.8 MB (20.13%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
+    // When we get back from recursion, the changes in the variables/vectors etc are undone as we roll back because the call stack is cleared. Because in every stack a new copy of variables are created (if you haven’t passed them from reference & operator)
+// SO, if you pass your variables with reference, then no new copy of that is made of recursive call. The same variable goes through all calls. In that case, you will have to explicitly remove element. 
+// I would suggest you to pass tmp by reference and see what happens . You will understand for sure
+
     vector<vector<int>> ans;
 
-    void solve(int sum, TreeNode* root, vector<int> &temp, int target) {
+    void solve(int sum, TreeNode* root, vector<int> temp, int target) {
         if(root == NULL) return;
 
         sum += root -> val;
@@ -10,14 +14,11 @@ public:
 
         if(root -> left == NULL && root -> right == NULL) {
             if(sum == target) ans.push_back(temp);
-            temp.pop_back(); // backtrack before returning
             return;
         }
 
         solve(sum, root -> left, temp, target);
         solve(sum, root -> right, temp, target);
-
-        temp.pop_back(); // backtrack
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
