7267fa286238cc56f29631c3d01fb7dcb1f110df
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-05 18:16:51 +0530

    Time: 27 ms (62.74%) | Memory: 11.5 MB (85.10%) - LeetSync

--- 1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
+++ 1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int ans = 0;
 
-    void solve(int index, int count, vector<int> &b, vector<vector<int>> &requests, int n) {
+    int solve(int index, vector<int> &b, vector<vector<int>> &requests, int n) {
         if(index == requests.size()) {
-            for(int i = 0; i < b.size(); i++) if(b[i] != 0) return; // this count is not valid, so jo abhi tk ans tha, vahi return krdo bss
-            ans = max(ans, count);
-            return;
+            for(int i = 0; i < b.size(); i++) if(b[i] != 0) return -1e9;
+            return 0; // no more requests left 
         }
 
         // we can either take the request or reject it
 
         // not take
-        solve(index + 1, count, b, requests, n);
+        int skip = 0 + solve(index + 1, b, requests, n);
 
         // take
         int from = requests[index][0], to = requests[index][1];  
 
         b[from]--, b[to]++;
-        solve(index + 1, count + 1, b, requests, n);
+        int take = 1 + solve(index + 1, b, requests, n);
         b[from]++, b[to]--; // backtrackk
+
+        return max(take, skip);
     }
 
     int maximumRequests(int n, vector<vector<int>>& requests) {
         vector<int> b(n, 0);
-        solve(0, 0, b, requests, n); // index, count  
+        int ans = solve(0, b, requests, n); // index, count  
         return ans;                              
     }
 };
