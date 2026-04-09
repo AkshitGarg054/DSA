8af2f935ea5198a7b333aa392b56de04f1640845
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-10 00:00:10 +0530

    Time: 275 ms (37.82%) | Memory: 192.8 MB (24.53%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    // For any number 'a', if any element is from (a - k, a + k) ...we can convert it to 'a'.
-    // So, for every ele in nums, we will check how many elements we can converge to that ele.
-    // Brute force will take O(n^2)
-    // To optimize it, we will create a freq vector which will store the freq of each ele.
-    // size of freq vector = value of maximum element in nums
-    // and to get the count of numbers in the range (l, r), we can do freq[r] - freq[l - 1] (in prefix sum array)
+    // DAT approach
 
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         int n = nums.size();
-        int maxi = *max_element(nums.begin(), nums.end());
+        int maxi = *max_element(nums.begin(), nums.end()) + k;
 
-        vector<int> freq(maxi + 1, 0);
-        for(auto ele : nums) freq[ele]++;
+        vector<int> diff(maxi + 2, 0);
+        unordered_map<int, int> mp;
 
-        // take cumulative sum
-        for(int i = 1; i <= maxi; i++) freq[i] += freq[i-1];
+        for(int i = 0; i < n; i++) {
+            int ele = nums[i];
+            mp[ele]++;
+
+            int l = max(0, ele - k);
+            int r = min(maxi, ele + k);
+
+            diff[l]++;
+            diff[r + 1]--;
+        }
+
+        // take cumulative sum of diff array
+        for(int i = 1; i < diff.size(); i++) diff[i] += diff[i - 1];
 
         int ans = 1;
 
-        // we can't loop from i = 0 to n, because the maximum freq element can also be the one, which does not even exists in nums array
-        // for example : nums = [88, 53], k = 27, numOperations = 2 (In this case, answer = 2, and maxi freq element is neither 88 nor 53)
         for(int ele = 1; ele <= maxi; ele++) {
-            int curr_freq = freq[ele] - freq[ele - 1]; // cumulative sum le liya na, so freq[ele - 1] subtract krna pdega
-            int r = min(maxi, ele + k);
-            int l = max(0, ele - k);
-
-            long long total_count = freq[r] - ((l > 0) ? freq[l - 1] : 0);
-            int required_operations = total_count - curr_freq;
-
-            int valid = min(numOperations, required_operations);
-            ans = max(ans, curr_freq + valid);
+            int required_ops = diff[ele] - mp[ele];
+            int valid = min(required_ops, numOperations);
+            ans = max(ans, valid + mp[ele]);
         }
 
         return ans;
