061003f1e78b4738d67c0a71b52b82260277310a
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-13 17:41:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (3.54%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+
+        vector<int> vec(code.begin(), code.end());
+        for(int i = 0; i < n; i++) vec.push_back(code[i]);
+
+        vector<int> prefix(2*n + 1);
+        prefix[0] = vec[0];
+        for(int i = 1; i < 2*n; i++) prefix[i] = prefix[i-1] + vec[i];
+
+        vector<int> ans(n, 0);
+
+        if(k > 0) {
+            for(int i = 0; i < n; i++) {
+                int sum = prefix[i + k] - prefix[i];
+                ans[i] = sum;
+            }
+        }
+        else if(k < 0) {
+            k = -k;
+            for(int i = n; i < 2*n; i++) {   
+                int sum = prefix[i-1] - prefix[i-k-1];
+                ans[i - n] = sum;
+            }
+        }
+        
+        return ans;
+    }
+};
