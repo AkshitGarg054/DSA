536de46df348cf464e934da9e0472a61c3b7e575
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-24 16:02:52 +0530

    Time: 51 ms (23.85%) | Memory: 27.8 MB (25.00%) - LeetSync

---
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int n;
+
+    long long solve(int index, int limit, vector<int> &prices) {
+        if(index == prices.size()) return 0;
+        int new_limit = min(n - 1, index + (index + 1)); // new limit becomes till next (index + 1) 
+        if(dp[index][limit] != -1) return dp[index][limit];
+
+        int buy = prices[index] + solve(index + 1, new_limit, prices); 
+
+        // buy for free only if the fruit is inside limit
+        int free = INT_MAX;
+        if(index <= limit) free = 0 + solve(index + 1, limit, prices);
+
+        return dp[index][limit] = min(buy, free);
+    }
+
+    int minimumCoins(vector<int>& prices) {
+        n = prices.size();
+        memset(dp, -1, sizeof(dp));
+
+        int cost = prices[0];
+        cost += solve(0, 1, prices); // start from 0st index; we can buy the next 1 (i + 1) fruits for free
+        return cost;
+    }
+};
