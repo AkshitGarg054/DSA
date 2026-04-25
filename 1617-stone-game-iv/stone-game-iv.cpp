d0dee6ba2c5e32154288b38af8c5394df8a0d0e9
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-25 17:36:51 +0530

    Time: 41 ms (48.02%) | Memory: 16.8 MB (29.60%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    bool solve(int n) {
+        if(n == 0) return false; // no stones left to pick
+        if(dp[n] != -1) return dp[n];
+
+        for(int k = 1; k*k <= n; k++) { // ry removing every perfect square k^2 < n
+            if(solve(n - k*k) == false) {
+                return dp[n] = true;
+            }
+        }
+
+        return dp[n] = false;
+    }
+
+    bool winnerSquareGame(int n) {
+        dp.resize(n + 1, -1);
+        return solve(n);
+    }
+};
