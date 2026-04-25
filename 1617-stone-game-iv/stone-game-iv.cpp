d51480cc0c2b3497ed792b7019f1b3d569baa7c9
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-25 17:48:02 +0530

    Time: 26 ms (88.11%) | Memory: 11 MB (46.39%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
+    // dp[i] = true → current player can win with i stones
+    // dp[i] = false → current player will lose with i stones
+    // From i, we can remove any perfect square: 1, 4, 9, 16....
+    // So, if there exists a move such that dp[i - square] == false, then dp[i] = true.
+    // Because we move to a losing state for the opponent, so we win.
+    // dp[0] = false.
+    // So, the simple approach is :
+    // Try removing every perfect square k^2 <= n
+    // If any move leads to a losing state for opponent → we win.
+
     vector<int> dp;
 
+    // this is top down approach
     bool solve(int n) {
         if(n == 0) return false; // no stones left to pick
         if(dp[n] != -1) return dp[n];
 
-        for(int k = 1; k*k <= n; k++) { // ry removing every perfect square k^2 < n
+        for(int k = 1; k*k <= n; k++) { // try removing every perfect square k^2 < n
             if(solve(n - k*k) == false) {
                 return dp[n] = true;
             }
@@ -16,7 +27,20 @@ public:
     }
 
     bool winnerSquareGame(int n) {
-        dp.resize(n + 1, -1);
-        return solve(n);
+        // dp.resize(n + 1, -1);
+        // return solve(n);
+
+        dp.resize(n + 1, 0); // all false initially
+
+        for(int i = 1; i <= n; i++) {
+            for(int k = 1; k*k <= i; k++) {
+                if(dp[i - k*k] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
     }
 };
