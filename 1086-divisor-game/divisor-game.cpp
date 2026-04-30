36497c04a9669903ba2822a8ab7078df9b02770e
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-04-30 20:19:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (7.15%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    // A player wins if they can move to a losing state for the opponent.
+    // define the state as : 
+    // dp[n] = true --> current player wins
+    // dp[n] = false --> current player loses
+    // dp[n] = true if ∃ x such that dp[n - x] == false
+
     bool divisorGame(int n) {
         vector<bool> dp(n + 1, false);
         dp[1] = false;
