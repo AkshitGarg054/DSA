f232a39cca82bd4f37a781cae964c7e4528d3644
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-01 17:33:17 +0530

    Time: 2 ms (69.31%) | Memory: 90.3 MB (35.98%) - LeetSync

---
+++ 1348-maximum-score-of-spliced-array/maximum-score-of-spliced-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int kadane(vector<int> &arr) {
+        int sum = 0, maxi = INT_MIN;
+        
+        for(int i = 0; i < arr.size(); i++) {
+            sum += arr[i];
+            maxi = max(maxi, sum);
+            if(sum < 0) sum = 0;
+        }
+
+        return maxi;
+    }
+
+    int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+
+        int sum1 = accumulate(nums1.begin(), nums1.end(), 0);
+        int sum2 = accumulate(nums2.begin(), nums2.end(), 0);
+
+        vector<int> diff1(n), diff2(n); // basically profit
+
+        for(int i = 0; i < n; i++) {
+            diff1[i] = nums2[i] - nums1[i]; // gain if we swap into nums1
+            diff2[i] = nums1[i] - nums2[i]; // gain if we swap into nums2
+        }
+
+        int max_gain1 = kadane(diff1); // using the subarray of nums2 into nums1
+        int max_gain2 = kadane(diff2);
+
+        return max(sum1 + max_gain1, sum2 + max_gain2);
+    }
+};
