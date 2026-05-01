5122e0613db80d4bf65bf2135d090c2fcd5f7684
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-02 02:12:39 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (27.97%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
-    // similar to search in rotated sorted array I
-    // the only problem here, that can arise is that, we will not able to recognize the sorted part
-    // when we will encounter arr[low] == arr[mid] == arr[high]
-    // so we just need to fix this condition
-    // as we are already checking arr[mid] == target, so if arr[mid] != target, then arr[low] and arr[high] also not equal to target 
-    // so, we can do, low + 1 and high - 1
-
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
-
         int low = 0, high = n - 1;
 
         while(low <= high) {
@@ -20,7 +12,7 @@ public:
                 continue;
             }
 
-            // check sorted part
+            // check sorted half
             if(nums[low] <= nums[mid]) {
                 if(target >= nums[low] && target < nums[mid]) high = mid - 1;
                 else low = mid + 1;
