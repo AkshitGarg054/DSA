28248067dc0640e01c0839acaad90334385a7aa1
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-03 16:18:33 +0530

    Time: 33 ms (88.88%) | Memory: 100.7 MB (91.47%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // pehle to position ke hisaab se sort kr lenge dono array ko combine krke
+    // Then, we will firstly just calculate ki har ek car ko target pe pohanchne me kitna time lgega
+    // and store these time in a separate time array
+    // we will traverse that time array and if time[i + 1] > time[i] hua, then we will merge them
+
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = speed.size();
+
+        vector<pair<int, int>> vec(n);
+        for(int i = 0; i < n; i++) vec[i] = {position[i], speed[i]};
+
+        sort(vec.begin(), vec.end()); // sort by position
+
+        vector<float> time(n);
+        for(int i = 0; i < n; i++) time[i] = (float)(target - vec[i].first) / vec[i].second; 
+
+        // for(int i = 0; i < n-1; i++) {
+        //     if(time[i] > time[i + 1]) count++; // aage wali car fast haii 
+        // }
+
+        int count = 0; 
+        float guard = time[n - 1]; // car at the neareast position to the target will reach first
+        // now we will compare all the incoming cars to this car
+
+        for(int i = n - 2; i >= 0; i--) {
+            float incoming = time[i];
+
+            if(incoming > guard) {
+                count++; // if the time of incoming car is greater then it will create a new fleet 
+                guard = incoming;
+            }
+        }
+
+        count++; // last fleet to reach target
+        return count;
+    }
+};
