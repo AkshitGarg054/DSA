4163350a8538acf67dd516d2a5fc0ac473a32c30
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-03 16:47:17 +0530

    Time: 82 ms (52.43%) | Memory: 166.9 MB (50.44%) - LeetSync

--- 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
+    // NOTE : Whenever we get a stack overflow error, it means our program used too much stack memory and it crashed.
+    // It usually happens due to :
+    // 1. infinite recursion or due to,
+    // 2. Allocating big arrays or objects locally, example : int arr[10000000];
+
     string largestMerge(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        string merge = "";
 
+        string merge = "";
         int i = 0, j = 0;
+
         while(i < n && j < m) {
-            if(word1[i] > word2[j]) merge += word1[i++];
-            else if(word1[i] < word2[j]) merge += word2[j++];
+            if(word1[i] > word2[j]) {
+                merge += word1[i];
+                i++;
+            }
+            else if(word1[i] < word2[j]) {
+                merge += word2[j];
+                j++;
+            }
+            // this part is tricky. (i thought that two branches bnenge isme to recursion lgana pdega)
+            // but always remember this trick of comparing the whole string in case of any confusion
+            else if(word1.substr(i) > word2.substr(j)) {
+                merge += word1[i];
+                i++;
+            }
             else {
-                if(word1.substr(i) > word2.substr(j)) merge += word1[i++];
-                else merge += word2[j++];
+                merge += word2[j];
+                j++;
             }
         }
 
-        while(i < n) merge += word1[i++];
-        while(j < m) merge += word2[j++];
+        if(i < n) merge += word1.substr(i);
+        if(j < m) merge += word2.substr(j);
 
         return merge;
     }
