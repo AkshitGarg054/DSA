ed8f365f3021d5d4aebdd72176626ad006fd1974
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-03 23:18:48 +0530

    Time: 17 ms (59.59%) | Memory: 22.2 MB (23.63%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    int solve(int index, int sum, int k, int target, vector<int> &nums, vector<int> &vis) {
-        if(k == 1) return true; // if only 1 subset left, remaining elements must form it
-        if(sum == target) return solve(0, 0, k-1, target, nums, vis);
-        if(index == nums.size()) return false; // we reached end, and no valid subset formed 
+    int n;
+    vector<int> dp; // again, here also, only 'mask' will be the dp state. (it takes into account, other things also)
 
-        // take
-        if(!vis[index] && sum + nums[index] <= target) {
-            vis[index] = 1;
-            if(solve(index + 1, sum + nums[index], k, target, nums, vis) == true) return true;
-            vis[index] = 0;
+    int solve(int mask, int sum, vector<int> &nums, int target, int k) {
+        if(k == 1) return true;
+        if(sum == target) return solve(mask, 0, nums, target, k - 1);
+        if(dp[mask] != -1) return dp[mask];
+
+        for(int i = 0; i < n; i++) {
+            if(!(mask & (1 << i))) { // if ith index is not vis
+
+                if(sum + nums[i] <= target) {
+                    if(solve(mask | (1 << i), sum + nums[i], nums, target, k)) return dp[mask] = true;
+                }
+            }
         }
 
-        // not take
-        return solve(index + 1, sum, k, target, nums, vis);
+        return dp[mask] = false;
     }
 
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int n = nums.size();
+        n = nums.size();
 
         int sum = 0;
         for(auto ele : nums) sum += ele;
         if(sum % k != 0) return false; 
 
         int target = sum / k;
-        // we just need to count the number of subsets with sum = target
-        // if count == k, then return true, else false
-        // but the problem in this approach is that :
-        // we are just counting, how many subsets of the array have sum = target (subsets can overlap)
-        // but we actually want the disjoint subsets where every element is used exactly once
-        vector<int> vis(n, 0); // therefore, we need a vis array
-
         sort(nums.rbegin(), nums.rend()); // sort in descending
-        if(nums[0] > target) return false;
+        if(nums[0] > target) return false; 
 
-        return solve(0, 0, k, target, nums, vis); // index, sum
+        dp.resize(1 << n, -1);
+        return solve(0, 0, nums, target, k); // mask, sum
     }
 };
