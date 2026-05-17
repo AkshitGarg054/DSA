6498fc8679b1f18e941b6054bb415fc6ce7c19e5
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-18 00:17:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (27.29%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    // See, hmne pehle ek question kra hua haii jisme hm sorted half dhundte the.
-    // But here, as we want the minimum element, so minimum can lie either in the unsorted half or it is the first element of the sorted half
-
     int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0, high = n - 1;
+        int l = 0;
+        int r = nums.size() - 1;
 
-        while(low < high) { // loop will stop when low == high (that is, only one element will left)
-            int mid = low + (high - low) / 2;
-            
-            if(nums[mid] < nums[high]) high = mid; // min can either be nums[mid] or it can be present in unsorted half
-            else if(nums[mid] > nums[high]) low = mid + 1; // as mid value is greater than high, so mid can't be min.
-            else high--; // duplicate handling --> shrink window
+        while(l < r) {
+
+            int mid = l + (r - l) / 2;
+            if(nums[mid] < nums[r]) {
+                r = mid;
+            }
+            else if(nums[mid] > nums[r]) {
+                l = mid + 1;
+            }
+            // duplicates
+            else {
+                r--;
+            }
         }
 
-        return nums[low]; // or nums[high]
+        return nums[l];
     }
 };
