cc44d6bede0f511292fbeffab2141184405de0df
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-24 21:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (25.38%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.cpp
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int n;
-    vector<vector<vector<int>>> dp;
-
-    int solve(int index, int count_a, int count_b, vector<vector<int>> &costs) {
-        if(count_a > n/2 || count_b > n/2) return 1e9;
-        if(index == n) {
-            if(count_a == (n / 2) && count_a == count_b) return 0;
-            else return 1e9;
-        }
-
-        if(dp[index][count_a][count_b] != -1) return dp[index][count_a][count_b];
-
-        int take_a = costs[index][0] + solve(index + 1, count_a + 1, count_b, costs);
-        int take_b = costs[index][1] + solve(index + 1, count_a, count_b + 1, costs);
-
-        return dp[index][count_a][count_b] = min(take_a, take_b);
-    }
-
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        n = costs.size();
+        int n = costs.size();
+        int ans = 0;
 
-        dp.resize(n, vector<vector<int>>(n, vector<int>(n, -1)));
-        return solve(0, 0, 0, costs);
+        // let's firstly send everyone to city A
+        for(int i = 0; i < n; i++) ans += costs[i][0];
+
+        // now, we need to move exactly N people from city A to B
+        // change in cost for each person = CostB - costA
+        // we will pick N people for which the change is most negative.
+
+        vector<int> diff(n);
+        for(int i = 0; i < n; i++) diff[i] = costs[i][1] - costs[i][0];
+        sort(diff.begin(), diff.end());
+
+        for(int i = 0; i < n/2; i++) ans += diff[i];
+        
+        return ans;
     }
 };
