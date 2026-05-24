f9e68817bee8d58f176ff22bf436837e18598db5
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-05-24 14:54:04 +0530

    Time: 3 ms (55.17%) | Memory: 12.4 MB (44.20%) - LeetSync

--- 1652-minimum-suffix-flips/minimum-suffix-flips.cpp
+++ 1652-minimum-suffix-flips/minimum-suffix-flips.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    // start iterating.
-    // at every position where s[i] and target[i] are not equal, we need to flip the suffix
-    // and every time, we need to compare with the updated s. (not the initial s)
-
     int minFlips(string target) {
         int n = target.size();
-        string s = string(n, '0');
-        int count = 0;
+        int flips = 0;
 
-        char next = s[0];
+        // if (flips == odd) then value at ith index in s is 1
+        // else if(flips == even) thn value at ith index in s is 0
         for(int i = 0; i < n; i++) {
-            if(next != target[i]) {
-                count++;
-                next = target[i];
-            }
+            char value = target[i];
+
+            if(flips % 2 != 0 && value == '0') flips++;
+            else if(flips % 2 == 0 && value == '1') flips++; 
         }
 
-        return count;
+        return flips;
     }
 };
