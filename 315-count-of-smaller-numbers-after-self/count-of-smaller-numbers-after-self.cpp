c31d69e33df76b0590c877772407f23efe09d365
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-04 13:07:56 +0530

    Time: 911 ms (28.14%) | Memory: 517.4 MB (8.74%) - LeetSync

---
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void merge(vector<pair<int, int>> &arr, int low, int mid, int high, vector<int> &count) {
+        int left = low;
+        int right = mid + 1;
+        vector<pair<int, int>> temp;
+
+        while(left <= mid && right <= high) {
+            if(arr[left].first <= arr[right].first) {
+                temp.push_back(arr[left]);
+
+                int freq = right - (mid + 1);
+                count[arr[left].second] += freq;
+
+                left++;
+            }
+            else {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid) {
+            count[arr[left].second] += right - (mid + 1);
+            temp.push_back(arr[left++]);
+        }
+
+        while(right <= high) temp.push_back(arr[right++]);
+
+        for(int i = low; i <= high; i++) arr[i] = temp[i - low];
+    }
+
+    void mergeSort(vector<pair<int, int>> &arr, int low, int high, vector<int> &count) {
+        if(low < high) {
+            int mid = low + (high - low) / 2;
+
+            mergeSort(arr, low, mid, count);
+            mergeSort(arr, mid + 1, high, count);
+            merge(arr, low, mid, high, count);
+        }
+    }
+
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<pair<int, int>> arr(n); // {value, index}
+        for(int i = 0; i < n; i++) arr[i] = {nums[i], i};
+
+        vector<int> count(n, 0);
+        mergeSort(arr, 0, n - 1, count);
+        return count;
+    }
+};
