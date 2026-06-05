05d2ffd915e4622b7f16d72ea4ce39a9ca669a7d
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-05 19:37:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (43.79%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-    // see, in basic binary search, we need indices of elements for assigning low = 0, and high = n-1
-    // so, in this 2d matrix also, hme indices chahiye honge
-    // so, whenever 2d matrix ke indices ki need hme dikh rhi ho, then we already have a trick to get the index of a cell of grid
-    // for a 1d index i --> 2d index => row = i / m , col = i % m 
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
@@ -13,13 +8,12 @@ public:
 
         while(low <= high) {
             int mid = low + (high - low) / 2;
-
             int r = mid / m;
             int c = mid % m;
 
             if(matrix[r][c] == target) return true;
-            else if(matrix[r][c] < target) low = mid + 1;
-            else high = mid - 1;
+            else if(matrix[r][c] > target) high = mid - 1;
+            else low = mid + 1;
         }
 
         return false;
