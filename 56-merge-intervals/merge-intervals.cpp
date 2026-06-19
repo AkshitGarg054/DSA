ca3835cab8661328abd0d0d7f1d2b4cd39557090
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-19 13:15:17 +0530

    Time: 3 ms (89.34%) | Memory: 23.9 MB (31.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
-
         vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
 
         int curr_l = intervals[0][0];
         int curr_r = intervals[0][1];
@@ -13,11 +12,11 @@ public:
             int check_l = intervals[i][0];
             int check_r = intervals[i][1];
 
-            if(check_l > curr_r) { // the interval don't overlap anymore, so start a new one
+            if(check_l > curr_r) { // don't overlap anymore, create a new interval
                 ans.push_back({curr_l, curr_r});
                 curr_l = check_l;
                 curr_r = check_r;
-            }   
+            }
             else curr_r = max(curr_r, check_r); // overlap
         }
 
