0bdfde68addd12e82c2f4b9f47c52ffed3b396aa
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-19 17:12:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.42%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-
-        // step 1: Find the first index i from the right such that nums[i] < nums[i + 1]
         int index = -1;
+        
+        // step 1
         for(int i = n-2; i >= 0; i--) {
             if(nums[i] < nums[i + 1]) {
                 index = i;
@@ -17,23 +17,22 @@ public:
             return;
         }
 
-        // step 2: Now, find the rightmost element greater than nums[i] in the suffix
-        int ele = nums[index];
+        // step 2
         int desired = nums[index + 1];
         int desired_index = index + 1;
 
-        for(int i = n - 1; i >= index + 2; i--) {
-            if(nums[i] > ele) {
+        for(int i = n-1; i >= index + 2; i--) {
+            if(nums[i] > nums[index]) {
                 desired = nums[i];
                 desired_index = i;
                 break;
             }
         }
 
-        // step 3: swap them
+        // step 3
         swap(nums[index], nums[desired_index]);
 
-        // reverse the suffix
+        // step 4
         reverse(nums.begin() + index + 1, nums.end());
     }
 };
