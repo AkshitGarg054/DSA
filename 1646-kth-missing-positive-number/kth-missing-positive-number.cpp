aeee8e74b1520d03f0b4bacff024b5d1bcac6513
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-19 22:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (60.20%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    // approach 2: using set
+    // approach 3: binary search
+    // In a sorted array, at index i, the number of missing positives before arr[i] is: arr[i] - (i + 1)
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = -1;
 
-        unordered_set<int> st(arr.begin(), arr.end());
-        int count = 0;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
 
-        for(int x = 1; ; x++) {
-            if(!st.count(x)) {
-                count++;
-                if(count == k) return x;
+            if(arr[mid] - (mid + 1) >= k) {
+                ans = mid;
+                high = mid - 1;
             }
+            else low = mid + 1;
         }
 
-        return -1;
+        if(ans == 0) return k;
+        if(ans == -1) return n + k;
+
+        int prev = ans - 1;
+        int prev_missing = arr[prev] - (prev + 1);
+        return arr[prev] + (k - prev_missing);
     }
 };
