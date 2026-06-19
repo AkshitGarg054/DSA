c23d3c83dc7129efafc38119f0de20bfb3112d4a
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-20 00:34:33 +0530

    Time: 94 ms (89.15%) | Memory: 128.4 MB (54.07%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -3,13 +3,15 @@ public:
     // we need the maximum time taken to reach any employee, that is, the max root-to-leaf path sum
     void dfs(int node, int curr, int &maxi, vector<vector<int>> &list, vector<int> &vis, vector<int> &informTime) {
         vis[node] = 1;
-        maxi = max(maxi, curr);
 
         for(auto &v: list[node]) {
             if(!vis[v]) {
+                maxi = max(maxi, curr);
                 dfs(v, curr + informTime[node], maxi, list, vis, informTime);
             }
         }
+
+        maxi = max(maxi, curr);
     }
 
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
