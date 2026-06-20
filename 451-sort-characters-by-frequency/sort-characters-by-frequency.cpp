7fdb0f1dd1538016cf68812580d5406fa8043d22
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-20 16:38:31 +0530

    Time: 63 ms (5.04%) | Memory: 10.4 MB (95.71%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    // like, we use a freq array of size 26 for lowercase letters,
-    // similarly, we can use an ASCII freq array of size 128 for uppercase and lowercase English letters and digits.
+    // approach 2: using custom comparator
     string frequencySort(string s) {
         int n = s.size();
 
-        vector<int> freq(128, 0);   
-        for(auto ch: s) freq[ch]++;
+        unordered_map<int, int> mp;
+        for(auto ch: s) mp[ch]++;
 
-        vector<pair<int, char>> vec;
-        for(int i = 0; i < 128; i++) {
-            if(freq[i] > 0) vec.push_back({freq[i], (char)i});
-        }
+        sort(s.begin(), s.end(), [&](char &a, char &b) {
+            if(mp[a] == mp[b]) return a > b;
+            return mp[a] > mp[b];
+        });
 
-        sort(vec.begin(), vec.end(), greater<>());
-
-        string ans = "";
-        for(auto &[f, ch]: vec) ans += string(f, ch);
-        return ans;
+        return s;
     }
 };
