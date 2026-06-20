b17555eedf450eebb281c1f6765aa1ddedad55e0
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-20 21:17:48 +0530

    Time: 67 ms (73.48%) | Memory: 173.2 MB (55.15%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,43 +1,37 @@
 class LRUCache {
 private:
-    unordered_map<int, pair<int, int>> mp; // key --> {value, last_used_time}
-    int timer;
     int capacity;
+    list<pair<int, int>> dll; // the list stores pairs of {key, val}
+    unordered_map<int, list<pair<int, int>>::iterator> mp; // the map links the key to the exact location (iterator) of the node in the list
 
 public:
     LRUCache(int capacity) {
         this -> capacity = capacity;
-        timer = 0;
     }
     
     int get(int key) {
-        if(mp.count(key)) {
-            timer++;
-            mp[key].second = timer;
-            return mp[key].first;
-        }
-        return -1;
+        if(!mp.count(key)) return -1;
+
+        // if key exists, move it to the front using splice
+        dll.splice(dll.begin(), dll, mp[key]); // splice(destination, source_list, element_iterator)
+        return mp[key] -> second; // return the value 
     }
     
     void put(int key, int value) {
-        timer++;
-        mp[key] = {value, timer};
-
-        if(mp.size() > capacity) { // evict the least recently used key
-            int mini = INT_MAX;
-            int required_key = -1;
-
-            for(auto [key, p]: mp) {
-                int k = key;
-                int t = p.second;
-
-                if(t < mini) {
-                    mini = t;
-                    required_key = k;
-                }
-            }
-
-            mp.erase(required_key);
+        if(mp.count(key)) {
+            mp[key] -> second = value;
+            dll.splice(dll.begin(), dll, mp[key]);
+            return;
         }
+
+        if(dll.size() == capacity) {
+            int lru_key = dll.back().first;
+            dll.pop_back();
+            mp.erase(lru_key);
+        }
+
+        // insert the new key-value pair at the front
+        dll.push_front({key, value});
+        mp[key] = dll.begin(); // store its iterator in the map
     }
 };
