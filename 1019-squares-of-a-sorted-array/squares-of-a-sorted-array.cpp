7506bee69a82e8abb7062ca19bb558ad33476f7c
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-21 14:34:28 +0530

    Time: 28 ms (6.01%) | Memory: 36.6 MB (5.09%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
+    // approach 3: using hashmap and tracking the maximum value
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
+
+        unordered_map<int, int> mp;
+        int maxi = INT_MIN;
+
+        for(auto x: nums) {
+            x = abs(x);
+            maxi = max(maxi, x);
+            mp[x]++;
+        }
+
         vector<int> ans;
 
-        for(int i = 0; i < n; i++) ans.push_back(nums[i] * nums[i]);
-        sort(ans.begin(), ans.end());
+        for(int val = 0; val <= maxi; val++) {
+            if(!mp.count(val)) continue;
+
+            while(mp[val] > 0) {
+                ans.push_back(val * val);
+                mp[val]--;
+            }
+        }
+
         return ans;
     }
 };
