6a7e3c6bbfcfee2c4c661f50bddd5d6c25857594
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-21 15:33:23 +0530

    Time: 1544 ms (5.01%) | Memory: 136.9 MB (95.52%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
 
-        vector<int> ans;
         int maxi = INT_MIN;
         int maxi_freq = 0;
 
@@ -15,47 +14,50 @@ public:
             else if(nums[i] == maxi) maxi_freq++;
         }
 
+        vector<int> ans;
         ans.push_back(maxi);
 
         int l = 0, r = k;
 
         while(r < n) {
-            // extend window
+            // extend window: incoming ele can be = maxi, > maxi, < maxi
             if(nums[r] > maxi) {
                 maxi = nums[r];
                 maxi_freq = 1;
-                l++; // to always maintain k size
+                l++; // no matter what nums[l] is, coz we got the new maxi
             }
             else if(nums[r] == maxi) {
                 if(nums[l] != maxi) maxi_freq++;
                 l++;
             }
             else { // nums[r] < maxi
-                if(nums[l] == maxi && maxi_freq > 1) {
-                    maxi_freq--;
-                    l++;
-                }
-                else if(nums[l] == maxi && maxi_freq == 1) {
-                    l++;
+                // there can be two cases: either nums[l] == maxi or != maxi
+                if(nums[l] != maxi) l++;
+                else { // nums[l] == maxi
+                    if(maxi_freq == 1) { // compute new maxi
+                        l++;
+                        maxi = INT_MIN;
+                        maxi_freq = 0;
 
-                    maxi = INT_MIN;
-                    maxi_freq = 0;
-
-                    for(int i = l; i <= r; i++) {
-                        if(nums[i] > maxi) {
-                            maxi = nums[i];
-                            maxi_freq = 1;
+                        for(int i = l; i <= r; i++) {
+                            if(nums[i] > maxi) {
+                                maxi = nums[i];
+                                maxi_freq = 1;
+                            }
+                            else if(nums[i] == maxi) maxi_freq++;
                         }
-                        else if(nums[i] == maxi) maxi_freq++;
+                    }
+                    else {
+                        maxi_freq--;
+                        l++;
                     }
                 }
-                else if(nums[l] != maxi) l++;
             }
 
             ans.push_back(maxi);
             r++;
         }
-
+        
         return ans;
     }
 };
