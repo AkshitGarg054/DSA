2b383e9b6492ce7d57bbdc27ecd55dc640aee09b
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-21 23:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (47.02%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
-    // Max Depth pattern.
+    int solve(TreeNode *p, TreeNode* q) {
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+        if(p -> val != q -> val) return false;
+
+        bool left = solve(p -> left, q -> left);
+        bool right = solve(p -> right, q -> right);
+
+        if(left && right) return true;
+        return false;
+    }
+
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == NULL && q == NULL) return true;
         if(p == NULL || q == NULL) return false;
-        return (p -> val == q -> val) && isSameTree(p -> left, q -> left) && isSameTree(p -> right, q -> right);
+
+        return solve(p, q);
     }
 };
