75a8440771eac8c52d922c4d82750f132e2cf2ed
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-23 16:19:30 +0530

    Time: 15 ms (30.63%) | Memory: 15.4 MB (22.43%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -5,22 +5,20 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        vector<vector<int>> vis(n, vector<int>(n, 0));
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
 
-        pq.push({grid[0][0], 0, 0}); 
+        pq.push({grid[0][0], 0, 0});
         vis[0][0] = 1;
 
         while(!pq.empty()) {
             auto it = pq.top();
             pq.pop();
-            int time = it[0];
-            int row = it[1];
-            int col = it[2];
 
-            if(row == n - 1 && col == n - 1) return time;
+            int val = it[0], row = it[1], col = it[2];
+            if(row == n - 1 && col == n - 1) return val;
 
-            for(auto &d : dirs) {
+            for(auto &d: dirs) {
                 int nr = row + d[0];
                 int nc = col + d[1];
 
@@ -28,7 +26,7 @@ public:
                 if(vis[nr][nc]) continue;
 
                 vis[nr][nc] = 1;
-                pq.push({max(grid[nr][nc], time), nr, nc});
+                pq.push({max(val, grid[nr][nc]), nr, nc});
             }
         }
 
