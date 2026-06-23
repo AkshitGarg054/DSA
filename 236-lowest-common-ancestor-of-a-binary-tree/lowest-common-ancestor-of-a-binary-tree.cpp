44826e77a8f702386a5eebdd3aef13cff7f739a2
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-23 12:38:17 +0530

    Time: 74 ms (6.06%) | Memory: 51.2 MB (5.06%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
+    // approach 2: Find the path from root node to p, and root node to q
+    // and then going till the prefix matches, to find the LCA
+
+    bool find_path(TreeNode* root, TreeNode* target, vector<TreeNode*> &path) {
+        if(root == NULL) return false;
+
+        path.push_back(root);
+        if(root == target) return true;
+        
+        bool left = find_path(root -> left, target, path);
+        bool right = find_path(root -> right, target, path);
+
+        if(left || right) return true; // if we get true from either left or right, then we are done, so we returned here so that dubara pop_back() na ho
+
+        path.pop_back(); // backtrack 
+        return false; 
+    }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) return NULL;
-        if(root -> val == p -> val || root -> val == q -> val) return root;
 
-        auto left = lowestCommonAncestor(root -> left, p, q);
-        auto right = lowestCommonAncestor(root -> right, p, q);
+        vector<TreeNode*> path_p, path_q;
 
-        if(left && right) return root;
+        find_path(root, p, path_p);
+        find_path(root, q, path_q);
+
+        TreeNode* ans = NULL;
+        int n = min(path_p.size(), path_q.size());
+
+        for(int i = 0; i < n; i++) {
+            if(path_p[i] == path_q[i]) ans = path_p[i];
+            else break;
+        }
         
-        return left ? left : right;
+        return ans;
     }
 };
