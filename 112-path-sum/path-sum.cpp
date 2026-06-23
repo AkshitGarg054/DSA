95301d2ab8349514a6a807e9c383611fc3887ba8
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-23 12:58:54 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (95.75%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    // at every node, subtract the current node value from targetSum, then check left and right subtree
-    // if you reach a leaf node, and remaining target becomes exactly that leaf value, then return true
+    int sum = 0;
+
+    bool solve(TreeNode* root, int target) {
+        if(root == NULL) return false;
+
+        sum += root -> val;
+        if(root -> left == NULL && root -> right == NULL) {
+            if(sum == target) return true;
+        } 
+
+        bool left = solve(root -> left, target);
+        bool right = solve(root -> right, target);
+        if(left || right) return true;
+
+        sum -= root -> val; // backtrack
+        return false;
+    }
 
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root == NULL) return false;
 
-        if(root -> left == NULL && root -> right == NULL) {
-            return root -> val == targetSum;
-        }
-
-        return hasPathSum(root -> left, targetSum - root -> val) || hasPathSum(root -> right, targetSum - root -> val);
+        return solve(root, targetSum);
     }
 };
