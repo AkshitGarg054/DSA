d35f5a9b9532e3259c116c8b3448b687ed9c353b
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-23 16:01:04 +0530

    Time: 8 ms (62.06%) | Memory: 42.9 MB (50.51%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    // we always try to pick (n+1) elements from the heap, kyuki hme n ka gap chahiye
-    // agar (n+1) elements mil rhe hnn, fir to boht badhiaa
-    // aur agar nhi mil rhee, to remaining needed elements ke liye idle process add krdo
+    // basically, jo task hmne currently kra haii, usko k time ke liye kahin side pe rakhdo
+    // aur firr k time pura hone ke baad vapis le aao
 
     int leastInterval(vector<char>& tasks, int n) {
         unordered_map<char, int> mp;
-        for(auto ch : tasks) mp[ch]++;
-
-        priority_queue<int> pq; // to extract maximum frequencies
-        for(auto it : mp) pq.push(it.second);
-
-        int count = 0;
+        for(auto ch: tasks) mp[ch]++;
         
-        while(!pq.empty()) {
-            vector<int> temp;
-            int cycle = n + 1;
+        priority_queue<int> pq; // max heap
+        for(auto it: mp) pq.push(it.second);
 
-            while(cycle && !pq.empty()) {
-                int f = pq.top();
-                pq.pop();
-                if(f - 1 > 0) temp.push_back(f - 1);
-                count++, cycle--;
+        queue<pair<int, int>> q; // {freq, time when it will go to pq}
+        int time = 0;
+
+        while(!pq.empty()) {
+            int maxi = pq.top();
+            pq.pop();
+
+            maxi--;
+            time++;
+            if(maxi > 0) q.push({maxi, time + n});
+
+            if(pq.empty() && q.size() > 0) {
+                int wait = q.front().second - time;
+                time += wait; // idle time added
             }
 
-            for(int x : temp) pq.push(x);
-            
-            if(!pq.empty()) count += cycle; // add remaining cycle as idle time if (n+1) unique processes were not there
+            if(q.front().second == time) {
+                pq.push(q.front().first);
+                q.pop();
+            }
         }
 
-        return count;
+        return time;
     }
 };
