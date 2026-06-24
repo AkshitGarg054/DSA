cfc760382aed745d52bc8a745c7a011a6f623f5e
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-24 16:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 37.4 MB (42.87%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    // This ques is the nicest application of BST Iterator.
+    // The idea is exactly like two sum in a sorted array: smallest + largest.
+    // Since inorder of BST is sorted, we create:
+    // Forward iterator: gives next element from the left side
+    // Backward iterator: gives prev element from the right side
+
     void storeLeft(TreeNode* root, stack<TreeNode*> &st) {
         if(root == NULL) return;
         while(root != NULL) {
