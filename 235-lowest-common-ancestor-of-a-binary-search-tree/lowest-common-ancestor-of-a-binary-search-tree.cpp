95e9e43df45d44d5e5993f04673de70cfceb2555
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-24 12:37:30 +0530

    Time: 26 ms (29.53%) | Memory: 23.6 MB (34.44%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL) return NULL;
-        if(root -> val == p -> val || root -> val == q -> val) return root;
-
-        auto left = lca(root -> left, p, q);
-        auto right = lca(root -> right, p, q);
-
-        if(left && right) return root;
-        return left ? left : right;
-    }
+    // The value of LCA of p and q will be between the values of p and q.
+    // that is, if p -> val = mini and q -> val = maxi
+    // then value of LCA will lie between [mini, maxi]
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) return NULL;
 
-        return lca(root, p, q);
+        int mini = min(p -> val, q -> val);
+        int maxi = max(p -> val, q -> val);
+
+        while(root != NULL) {
+            if(root -> val < mini) root = root -> right;
+            else if(root -> val > maxi) root = root -> left;
+            else break; // find the first value between [mini, maxi]
+        }
+
+        return root;
     }
 };
