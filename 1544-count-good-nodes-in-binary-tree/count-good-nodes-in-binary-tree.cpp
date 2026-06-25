552195a8a05b8519fe7a5f36730a49daed28f2a4
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-25 23:45:33 +0530

    Time: 97 ms (61.64%) | Memory: 88.5 MB (30.26%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int count = 0;
 
-    void solve(TreeNode* curr, int maxi, int &ans) {
-        if(curr == NULL) return;
+    void solve(TreeNode* root, int maxi) {
+        if(root == NULL) return;
 
-        if(curr -> val >= maxi) {
-            maxi = curr -> val;
-            ans++;
+        if(root -> val >= maxi) {
+            maxi = root -> val;
+            count++;
         }
 
-        solve(curr -> left, maxi, ans);
-        solve(curr -> right, maxi, ans);
-    } 
+        solve(root -> left, maxi);
+        solve(root -> right, maxi); // backtracking is internally handled by recursion here (try visualizing)
+    }
 
     int goodNodes(TreeNode* root) {
-        int ans = 0;
-        int maxi = INT_MIN;
-        solve(root, maxi, ans);
-        return ans;
+        if(root == NULL) return 0;
+        if(root -> left == NULL && root -> right == NULL) return 1;
+
+        solve(root, INT_MIN);
+        return count;
     }
 };
