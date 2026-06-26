a668b59f627d7cc59b1c5364b231ecf1ffae3d3e
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-26 21:30:48 +0530

    Time: 738 ms (14.32%) | Memory: 33.6 MB (13.58%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    // Why we need to mark the visited??
+    // If a word has already been discovered by BFS once, there is never any benefit in discovering it again later.
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        if(!st.count(endWord)) return 0;
+        st.insert(beginWord);
+
+        queue<string> q;
+        q.push(beginWord);
+        int level = 1; // to count beginWord
+
+        while(!q.empty()) {
+            int sz = q.size();
+            
+            while(sz--) {
+                string curr = q.front(); 
+                q.pop();
+
+                if(curr == endWord && st.count(curr)) return level;
+                st.erase(curr); // works as a visited marker (to prevent hit -> hot -> hit -> hot....)
+
+                for(int i = 0; i < curr.size(); i++) {
+                    int ch = curr[i]; 
+
+                    for(char c = 'a'; c <= 'z'; c++) {
+                        if(ch == c) continue;
+                        curr[i] = c; 
+                        if(st.count(curr)) q.push(curr);
+                    }
+
+                    curr[i] = ch; // undo 
+                }
+            }
+
+            level++;
+        }
+
+        return 0;
+    }
+};
