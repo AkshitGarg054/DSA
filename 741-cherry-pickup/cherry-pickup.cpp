58e00575922a02f76c7c32982ace41bdf73ddaa7
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-29 02:35:11 +0530

    Time: 2444 ms (5.05%) | Memory: 511.3 MB (10.09%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    int n;
-    vector<vector<vector<int>>> dp;
+    int n, m;
+    vector<vector<vector<vector<int>>>> dp;
 
-    int solve(int r1, int c1, int r2, vector<vector<int>> &grid) {
-        int c2 = r1 +  c1 - r2; // (r1 + c1 == r2 +  c2)
+    int solve(int r1, int c1, int r2, int c2, vector<vector<int>>& grid) {
+        if(r1 < 0 || r1 >= n || c1 < 0 || c1 >= m) return -1e9;
+        if(r2 < 0 || r2 >= n || c2 < 0 || c2 >= m) return -1e9;
+        if(grid[r1][c1] == -1 || grid[r2][c2] == -1) return -1e9;
+        if(r1 == n-1 && c1 == m-1) return grid[r1][c1]; // both are taking same no. of steps, so both will reach dest simultaneously
+        if(dp[r1][c1][r2][c2] != 1e9) return dp[r1][c1][r2][c2];
 
-        if(r1 >= n || c1 >= n || r2 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1) return -1e9;
-        if(r1 == n-1 && c1 == n-1) return grid[r1][c1];
-        if(dp[r1][c1][r2] != -1) return dp[r1][c1][r2];
+        int ans = -1e9;
 
-        int ans = 0;
-        if(r1 == r2 && c1 == c2) ans += grid[r1][c1];
-        else ans += grid[r1][c1] + grid[r2][c2];
+        if(r1 == r2 && c1 == c2) { // both are on the same cell
+            int dd = grid[r1][c1] + solve(r1 + 1, c1, r2 + 1, c2, grid);
+            int rr = grid[r1][c1] + solve(r1, c1 + 1, r2, c2 + 1, grid);
+            int dr = grid[r1][c1] + solve(r1 + 1, c1, r2, c2 + 1, grid);
+            int rd = grid[r1][c1] + solve(r1, c1 + 1, r2 + 1, c2, grid);
+            ans = max({ans, dd, rr, dr, rd});
+        }
+        else {
+            int dd = grid[r1][c1] + grid[r2][c2] + solve(r1 + 1, c1, r2 + 1, c2, grid);
+            int rr = grid[r1][c1] + grid[r2][c2] + solve(r1, c1 + 1, r2, c2 + 1, grid);
+            int dr = grid[r1][c1] + grid[r2][c2] + solve(r1 + 1, c1, r2, c2 + 1, grid);
+            int rd = grid[r1][c1] + grid[r2][c2] + solve(r1, c1 + 1, r2 + 1, c2, grid);
+            ans = max({ans, dd, rr, dr, rd});
+        }
 
-        int dd = ans + solve(r1+1, c1, r2+1, grid); // down, down
-        int rr = ans + solve(r1, c1+1, r2, grid);   // right, right
-        int dr = ans + solve(r1+1, c1, r2, grid);   // down, right
-        int rd = ans + solve(r1, c1+1, r2+1, grid);  // right, down
-        
-        return dp[r1][c1][r2] = max({dd, rr, dr, rd}); 
+        return dp[r1][c1][r2][c2] = ans;
     }
- 
+
     int cherryPickup(vector<vector<int>>& grid) {
         n = grid.size();
-        if(grid[n-1][n-1] == -1 || grid[0][0] == -1) return 0;
+        m = grid[0].size();
+        if(grid[0][0] == -1 || grid[n-1][m-1] == -1) return 0;
 
-        dp.resize(51, vector<vector<int>>(51, vector<int>(51, -1)));
+        dp.assign(51, vector<vector<vector<int>>>(51, vector<vector<int>>(51, vector<int>(51, 1e9))));
+        int ans = solve(0, 0, 0, 0, grid);
 
-        int ans = solve(0, 0, 0, grid); // r1, c1, r2 (r1 + c1 == r2 +  c2)
-        return max(0, ans);
+        if(ans < 0) return 0;
+        return ans;
     }
 };
