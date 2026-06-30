a228c132bfb015df237683d63708265459811999
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-06-30 18:47:27 +0530

    Time: 39 ms (83.89%) | Memory: 12 MB (43.13%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    int n;
-    vector<int> dp;
+    int minCut(string s) {
+        int n = s.size();
 
-    bool check(string &s) {
-        int l = 0, r = s.size() - 1;
-        while(l < r) {
-            if(s[l] != s[r]) return false;
-            l++, r--;
-        }
-        return true;
-    }
+        // precompute palindromes 
+        vector<vector<bool>> isPal(n, vector<bool>(n, false)); // isPal[i][j] tells if s[i..j] is a palindrome or not
 
-    int solve(int i, string &s) {
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-
-        int mini = 1e9;
-        string temp = "";
-
-        for(int j = i; j < n; j++) {
-            temp += s[j];
-            if(check(temp)) {
-                int palindromes = 1 + solve(j + 1, s);
-                mini = min(mini, palindromes);
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = i; j < n; j++) {
+                if(s[i] == s[j] && (j - i <= 2 || isPal[i + 1][j - 1])) {
+                    isPal[i][j] = true;
+                }
             }
         }
 
-        return dp[i] = mini;
-    }
+        // calculate minimum cuts
+        vector<int> cuts(n, 0); // cuts[i] stores the min cuts needed for the prefix s[0...i]
 
-    int minCut(string s) {
-        n = s.size();
+        for(int i = 0; i < n; i++) {
+            if(isPal[0][i] == true) { // if the prefix upto i is a palindrome, then no cuts needed till i
+                cuts[i] = 0;
+            }
+            else {
+                cuts[i] = i; // max possible cuts till prefix i
 
-        dp.assign(n, -1);
-        int ans = solve(0, s);
-        ans = ans - 1; // coz we counted the number of palindromes
-        return ans;
+                // try making a cut after every index j
+                for(int j = 0; j < i; j++) {
+                    if(isPal[j + 1][i]) { // check if right side is a palindrome
+                        cuts[i] = min(cuts[i], cuts[j] + 1);
+                    }
+                }
+            }
+        }
+
+        return cuts[n - 1];
     }
 };
