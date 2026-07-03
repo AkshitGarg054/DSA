efa2b3c6a34bca3f66bc73a3daa6e5d7aff3514d
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-03 20:44:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (41.92%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
-    // generally, we start the pointers from beginning.
-    // But here, if we start from beginning, then everytime we need to shift the elements of nums1 forward.
-    // instead, we can see that we have an extra space at the end of nums1
-    // so we will start the pointers from backward and keep building the sorted array
-
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = n + m - 1;
+        int i = m-1, j = n-1;
+        int k = m + n - 1;
 
         while(i >= 0 && j >= 0) {
             if(nums1[i] > nums2[j]) {
@@ -21,10 +15,6 @@ public:
             }
         }
 
-        // if j is still left
-        while(j >= 0) {
-            nums1[k] = nums2[j];
-            k--, j--;
-        }
+        while(j >= 0) nums1[k--] = nums2[j--];
     }
 };
