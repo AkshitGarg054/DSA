1dfad621671faa8ed2ae95c0965183eb7ab34d71
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-03 17:18:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (81.52%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -4,25 +4,28 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
+        int top = 0, bottom = n - 1;
+        int left = 0, right = m - 1;
         vector<int> ans;
-        int top = 0, left = 0, right = m - 1, bottom = n - 1;
 
         while(top <= bottom && left <= right) {
-            // top row
-            for(int j = left; j <= right; j++) ans.push_back(matrix[top][j]);
+            for(int j = left; j <= right; j++) {
+                ans.push_back(matrix[top][j]);
+            }
+
             top++;
 
-            // right column
-            for(int i = top; i <= bottom; i++) ans.push_back(matrix[i][right]);
+            for(int i = top; i <= bottom; i++) {
+                ans.push_back(matrix[i][right]);
+            }
+
             right--;
 
-            // bottom row
             if(top <= bottom) {
                 for(int j = right; j >= left; j--) ans.push_back(matrix[bottom][j]);
                 bottom--;
             }
-
-            // left column
+            
             if(left <= right) {
                 for(int i = bottom; i >= top; i--) ans.push_back(matrix[i][left]);
                 left++;
