50f1f9658e36bc469e1b19f67c231fc0444dd60f
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-04 13:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.49%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    // in this question, one half will always be sorted, and we just need to find out which half is sorted
-
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-
         int low = 0, high = n - 1;
-
+        
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            if(target == nums[mid]) return mid;
-            
-            // check which half is sorted
-            // if left sorted
-            if(nums[low] <= nums[mid]) {
+            if(nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid]) { // left half sorted
                 if(target >= nums[low] && target < nums[mid]) high = mid - 1;
                 else low = mid + 1;
             }
-            else if(nums[mid] <= nums[high]) { // if right sorted
-                if(target > nums[mid] && target <= nums[high]) low = mid + 1;
+            else {
+                if(target <= nums[high] && target > nums[mid]) low = mid + 1;
                 else high = mid - 1;
             }
         }
