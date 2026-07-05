ece9a45b6dcb90b49da5159e53363b602eb50aec
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-05 15:22:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (48.76%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
+    // inplace ke liye hme pointers hi rakhne hote hn.
+    // So, we need three pointers here.
+
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int mid = 0, high = n - 1;
-        int low = 0;
+        int low = 0, mid = 0;
+        int high = n - 1;
 
         while(mid <= high) {
             if(nums[mid] == 0) {
                 swap(nums[low], nums[mid]);
-                mid++, low++;
+                low++, mid++;
             }
             else if(nums[mid] == 1) mid++;
             else {
                 swap(nums[mid], nums[high]);
                 high--;
             }
-        }
+        } 
     }
 };
