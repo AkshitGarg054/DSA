f4a1b34a3a03df0bff407a86d56c496dc4c02a44
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-05 20:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (39.05%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,53 +1,51 @@
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* next = NULL;
+    // i think we can reverse each sublist and then join the last node of reversed sublist to the remaining list.
 
-        while(head != NULL) {
-            next = head -> next;
-            head -> next = prev;
-            prev = head;
-            head = next;
+    ListNode* reverse(ListNode* head) {
+        if(head == NULL || head -> next == NULL) return head;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* nextNode = head -> next;
+
+        while(curr != NULL) {
+            curr -> next = prev;
+            prev = curr;
+            curr = nextNode;
+            if(nextNode != NULL) nextNode = nextNode -> next;
         }
 
-        head = prev;
-        return head;
+        return prev;
     }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head == NULL || head -> next == NULL || k == 1) return head;
-
-        ListNode* temp = head;
-        int count = 0;
-        while(temp != NULL) {
-            count++;
-            temp = temp -> next;
-        }
-
-        int groups = count / k;
-
-        ListNode* dummy = new ListNode(-1);
+        if(head == NULL || head -> next == NULL) return head;
+        ListNode* dummy = new ListNode(0);
         dummy -> next = head;
-        ListNode* prev = dummy; // prev pointer points to the end of prev segment
-        ListNode* curr = head; //points to the current segment that is to be reversed
-        ListNode* next = NULL; //points to the start of next segment of current segment
 
-        while(groups) {
-            ListNode* start = head; //starting node of a segment
-            ListNode* end = head; // end node of a segment
-            for(int i = 0; i < k-1; i++) end = end -> next;
+        ListNode* tempHead = head;
+        ListNode* prevNode = dummy;
 
-            next = end -> next;
-            end -> next = NULL;
-            prev -> next = reverseList(start);
+        while(tempHead != NULL) {
+            int count = 1;
+            ListNode* node = tempHead;
+            while(count < k && node -> next != NULL) {
+                node = node -> next;
+                count++;
+            }
 
-            start -> next = next;
-            prev = start;
-            curr = next;
-            head = next; 
+            if(count < k) break;
 
-            groups--;
+            ListNode* nextNode = node -> next;
+            node -> next = NULL; // a subList has is created
+
+            ListNode* prevHead = tempHead; 
+            ListNode* newHead = reverse(tempHead);
+            prevHead -> next = nextNode;
+            prevNode -> next = newHead;
+
+            prevNode = prevHead;
+            tempHead = nextNode;
         }
 
         return dummy -> next;
