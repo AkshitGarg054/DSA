60d80c9a39d16c9bb025f8bf961d41b3861ee423
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-06 15:04:35 +0530

    Time: 7 ms (6.73%) | Memory: 17 MB (66.24%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -2,33 +2,28 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         int n = tokens.size();
-        stack<string> st;
-        
-        for(int i = 0; i < n; i++) {
+        stack<int> st;
+
+        int i = 0;
+        while(i < n) {
             string curr = tokens[i];
 
-            if(curr != "+" && curr != "-" && curr != "*" && curr != "/") {
-                st.push(curr);
-                continue;
-            }
-
-            if(st.size() >= 2) {
-                int num2 = stoi(st.top());
+            if(curr != "+" && curr != "-" && curr != "*" && curr != "/") st.push(stoi(curr));
+            else {
+                int num2 = st.top();
                 st.pop();
-                int num1 = stoi(st.top());
+                int num1 = st.top();
                 st.pop();
 
-                int ans;
-
-                if(curr == "+") ans = num1 + num2;
-                else if(curr == "-") ans = num1 - num2;
-                else if(curr == "*") ans = num1 * num2;
-                else if(curr == "/") ans = num1 / num2;
-
-                st.push(to_string(ans));
+                if(curr == "+") st.push(num1 + num2);
+                else if(curr == "-") st.push(num1 - num2);
+                else if(curr == "/") st.push(num1 / num2);
+                else if(curr == "*") st.push(num1 * num2);
             }
+
+            i++;
         }
 
-        return stoi(st.top());
+        return st.top();
     }
 };
