c0e8e0abdc471875960c8af6cfa1fd45cc5a912f
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-07 16:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (12.06%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    double solve(double x, long long power) {
-        if(power == 0) return 1;
+    double solve(double x, long long n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
 
-        double half = solve(x, power / 2);
-        if(power % 2 == 0) return half * half;
-        else return half * half * x;
+        double half = solve(x, n / 2);
+        if(n % 2 == 0) return half * half;
+        return half * half * x;
     }
 
     double myPow(double x, int n) {
-        long long power = n;
-        if(power < 0) {
+        long long N = n;
+        if(N < 0) {
             x = 1 / x;
-            power = -power;
+            N = -N; // if n is INT_MIN, then -n will get overflow, so we need n to be long long
         }
 
-        return solve(x, power);
+        return solve(x, N);
     }
 };
