d76dd411a10a5561155fe3fe8fdc2b59b50401fe
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-08 16:35:23 +0530

    Time: 336 ms (12.78%) | Memory: 96.1 MB (22.70%) - LeetSync

--- 472-concatenated-words/concatenated-words.cpp
+++ 472-concatenated-words/concatenated-words.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+    // The only approach i can think of is to check every word using "extend" and "new" method,
+    // whether that word can be formed by the combination of other given words or not
     unordered_set<string> st;
+    vector<vector<int>> dp;
 
-    bool solve(int index, string &s, vector<int> &dp) {
-        if(index == s.size()) return true;
-        if(dp[index] != -1) return dp[index];
+    bool solve(int i, int j, string &word) {
+        if(i >= word.size() && j >= word.size()) return true;
+        if(j >= word.size()) return false;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        string temp = "";
-        for(int i = index; i < s.size(); i++) {
-            temp += s[i];
-            if(st.count(temp)) {
-                if(solve(i + 1, s, dp) == true) return dp[index] = true;
-            }
+        st.erase(word); // so that a word does not match to itself
+        string sub = word.substr(i, j - i + 1);
+
+        if(st.count(sub)) {
+            if(solve(i, j + 1, word)) return dp[i][j] = true; // extend
+            if(solve(j + 1, j + 1, word)) return dp[i][j] = true; // make new
+        }
+        else {
+            if(solve(i, j + 1, word)) return dp[i][j] = true; // extend
         }
 
-        return dp[index] = false;
+        st.insert(word); // undo
+        return dp[i][j] = false;
     }
 
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        for(auto word : words) st.insert(word);
+        int n = words.size();
         vector<string> ans;
 
-        for(string word : words) {
-            vector<int> dp(word.size(), -1);
-        
-            st.erase(word); // remove the word itself from the set
-            if(solve(0, word, dp) == true) ans.push_back(word); 
-            st.insert(word); // add the word back
+        for(auto &word: words) st.insert(word);
+
+        for(int i = 0; i < n; i++) {
+            string word = words[i];
+            int m = word.size();
+
+            dp.assign(m + 1, vector<int>(m + 1, -1));
+            if(solve(0, 0, word)) ans.push_back(word);
         }
 
         return ans;
