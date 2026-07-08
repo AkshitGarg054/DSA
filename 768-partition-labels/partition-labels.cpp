82b340277031df35c222a3a3f46fd59c007f5778
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-08 13:38:55 +0530

    Time: 3 ms (24.96%) | Memory: 8.8 MB (86.10%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
+    // This seems very similar to jump game 2.
+    // Let's say we encountered a char ch, then we need to iterate till the last ch in the string, 
+    // and also keep seeing the range of inbetween elements, and have to choose the maximum one.
+
     vector<int> partitionLabels(string s) {
         int n = s.size();
+        vector<int> ans;
 
-        unordered_map<char, int> mp;
+        unordered_map<char, int> mp; // stores the last position of every character
         for(int i = n-1; i >= 0; i--) {
-            char ch = s[i];
-            if(!mp.count(ch)) mp[ch] = i;
+            if(!mp.count(s[i])) mp[s[i]] = i;
         }
 
-        vector<int> ans;
-        for(int i = 0; i < n; ) {
-            int j = i;
-            int maxi = mp[s[i]];
+        int i = 0;
+        while(i < n) {
+            char ch = s[i];
 
+            int j = i + 1;
+            int maxi = mp[ch];
             while(j <= maxi) {
                 maxi = max(maxi, mp[s[j]]);
                 j++;
@@ -22,7 +27,7 @@ public:
             ans.push_back(maxi - i + 1);
             i = maxi + 1;
         }
-        
+
         return ans;
     }
 };
