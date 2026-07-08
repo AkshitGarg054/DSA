3790f30f15d242e6141f6536005db3ac8b12c196
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-08 14:29:59 +0530

    Time: 85 ms (40.67%) | Memory: 148.5 MB (64.30%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,24 +1,24 @@
 class MedianFinder {
 public:
-    priority_queue<int> max_heap;
-    priority_queue<int, vector<int>, greater<>> min_heap;
+    priority_queue<int> max_heap; // max heap is used to store the smaller half of the numbers
+    priority_queue<int, vector<int>, greater<>> min_heap; // used to store the larger half of the numbers
 
     MedianFinder() {
-
+        
     }
     
     void addNum(int num) {
-        if(max_heap.empty() && min_heap.empty()) max_heap.push(num);
+        if(max_heap.empty() && min_heap.empty()) max_heap.push(num); // can push into min heap also
         else {
-            if(max_heap.top() < num) min_heap.push(num);
+            if(num > max_heap.top()) min_heap.push(num); // coz every element in max_heap should be <= every element in min_heap and max_heap.top() is largest ele of smaller half
             else max_heap.push(num);
         }
 
         int n = max_heap.size();
         int m = min_heap.size();
 
-        if(abs(n - m) == 2) { // got unbalanced
-            if(n > m) { // pop from n and push into m
+        if(abs(n - m) == 2) { // became unbalanced
+            if(n > m) { // pop from max_heap and push into min_heap
                 int ele = max_heap.top();
                 max_heap.pop();
                 min_heap.push(ele);
@@ -32,9 +32,9 @@ public:
     }
     
     double findMedian() {
-        double median;
         int n = max_heap.size();
         int m = min_heap.size();
+        double median;
 
         if((n + m) % 2 == 0) median = (max_heap.top() + min_heap.top()) / 2.0;
         else {
