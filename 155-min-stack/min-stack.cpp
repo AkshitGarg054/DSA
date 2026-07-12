85c16dc9d439031f33420383edf7157c88f4b28c
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-12 23:11:27 +0530

    Time: 65 ms (26.96%) | Memory: 151.3 MB (26.68%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,25 +1,39 @@
 class MinStack {
 public:
-    stack<pair<int, int>> st; // the first is the actual element, second is the minimum
+    stack<long long> st; // stores (value - mini)
+    long long mini;
 
     MinStack() {
-        
+        mini = INT_MAX;
     }
     
     void push(int value) {
-        if(st.size() > 0) st.push({value, min(value, st.top().second)});
-        else st.push({value, value});
+        if(st.empty()) {
+            st.push(0);
+            mini = value;
+        }
+        else {
+            long long diff = value - mini;
+            st.push(diff);
+            if(diff <= 0) mini = value; 
+            else mini = mini;
+        }
     }
     
     void pop() {
+        if(st.top() <= 0) { // it means, mini was st.top(), so now we need to update the mini to old minimum.
+            mini = mini - st.top(); // coz, top = value - mini, and mini = value
+        }
+
         st.pop();
     }
     
     int top() {
-        return st.top().first;
+        if(st.top() <= 0) return mini;
+        return st.top() + mini;
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
