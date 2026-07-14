a4d08aa29f337ffc41e5d75d3a92ef1554f9547f
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-14 13:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (80.48%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
-    // to convert a char to a string, we do: string(1, ch) ...not to_string(ch)
+    // We need to pass the index by reference as we want all the recursive calls to share the same index.
+
+    string solve(int &index, string &s) {
+        string temp = "";
+
+        while(index < s.size() && s[index] != ']') {
+
+            if(isdigit(s[index])) {
+                int freq = 0;
+                while(isdigit(s[index])) {
+                    freq = freq * 10 + (s[index] - '0');
+                    index++;
+                }
+
+                index++; // skip '['
+                string curr = solve(index, s);
+                index++; // skip ']'
+                while(freq--) temp += curr;
+            }
+            else if(s[index] >= 'a' && s[index] <= 'z') {
+                while(s[index] >= 'a' && s[index] <= 'z') temp += s[index++];
+            }
+        }
+
+        return temp;
+    }
 
     string decodeString(string s) {
         int n = s.size();
-        stack<string> st; // as the digits range from 1 to 300
-        int num = 0;
-
-        // input: 3[a2[bc]]
-        for(int i = 0; i < s.size(); i++) {
-            char ch = s[i];
-
-            if(s[i] >= '0' && s[i] <= '9') num = num*10 + (ch - '0'); 
-            else if(s[i] == '[') {
-                st.push(to_string(num));
-                num = 0;
-            }
-            else if(s[i] >= 'a' && s[i] <= 'z') st.push(string(1, ch));
-            else if(s[i] == ']') {
-                string temp = "";
-                // pop till we find a number
-                while(!st.empty() && (st.top()[0] >= 'a' && st.top()[0] <= 'z')) {
-                    temp += st.top();
-                    st.pop();
-                }
-
-                // temp = "cb"
-                reverse(temp.begin(), temp.end()); // bc
-                int freq = stoi(st.top());
-                st.pop();
-
-                string duplicated = "";
-                for(int i = 0; i < freq; i++) duplicated += temp; // duplicated = "bcbc"
-                for(int i = 0; i < duplicated.size(); i++) st.push(string(1, duplicated[i]));
-            }
-        }
-
-        string ans = "";
-        while(!st.empty()) {
-            ans += st.top();
-            st.pop();
-        }
-
-        reverse(ans.begin(), ans.end());
+        int index = 0;
+        string ans = solve(index, s);
         return ans;
     }
 };
