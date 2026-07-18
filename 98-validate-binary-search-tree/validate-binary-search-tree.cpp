7d264f6a863162d67029deba1b32f12948eecb9d
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-18 16:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (45.35%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool check(TreeNode* root, long long low, long long high) {
+    bool solve(TreeNode* root, long long low, long long high) {
         if(root == NULL) return true;
-        if(root -> val <= low || root -> val >= high) return false; // value should remain between low and high
-        
-        bool left = check(root -> left, low, root -> val);
-        bool right = check(root -> right, root -> val, high);
+        if(root -> val <= low || root -> val >= high) return false; // every node's value must lie between low and high
+
+        bool left = solve(root -> left, low, root -> val);
+        bool right = solve(root -> right, root -> val, high);
 
         return left && right;
     }
 
     bool isValidBST(TreeNode* root) {
         if(root == NULL) return true;
-        return check(root, LLONG_MIN, LLONG_MAX);
+        return solve(root, LLONG_MIN, LLONG_MAX);
     }
 };
