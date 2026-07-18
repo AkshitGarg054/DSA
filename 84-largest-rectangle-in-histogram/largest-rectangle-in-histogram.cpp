810b34f14b69ebd7070e3cd1de2abfa1d9389b32
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-18 19:45:59 +0530

    Time: 31 ms (26.01%) | Memory: 81.2 MB (79.64%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    // for every rectangle, we want the next smaller and the previous smaller
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         stack<int> st;
-        int ans = INT_MIN;
+        int maxi = INT_MIN;
 
         for(int i = 0; i < n; i++) {
             while(!st.empty() && heights[st.top()] > heights[i]) {
                 int curr = st.top();
                 st.pop();
-                int nse = i;
-                int pse = st.empty() ? -1 : st.top();
-                int width = nse - pse - 1;
 
-                int area = width * heights[curr];
-                ans = max(ans, area);
+                int nse = i;
+                int pse = (st.empty() ? -1 : st.top());
+
+                int width = nse - pse - 1;
+                int h = heights[curr]; 
+                maxi = max(maxi, h * width);
             }
 
             st.push(i);
@@ -24,14 +24,15 @@ public:
         while(!st.empty()) {
             int curr = st.top();
             st.pop();
-            int nse = n;
-            int pse = st.empty() ? -1 : st.top();
-            int width = nse - pse - 1;
 
-            int area = width * heights[curr];
-            ans = max(ans, area);
+            int nse = n;
+            int pse = (st.empty() ? -1 : st.top());
+            
+            int width = nse - pse - 1;
+            int h = heights[curr]; 
+            maxi = max(maxi, h * width);
         }
 
-        return ans;
+        return maxi;
     }
 };
