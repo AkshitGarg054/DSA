72973506b2dd1d8f6446b8e4757e70968e5beb6a
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-22 19:01:26 +0530

    Time: 59 ms (30.24%) | Memory: 18.8 MB (39.33%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -3,15 +3,14 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int row = 0; // contains minimum values of the entire matrix
-        int col = m - 1; // contains maximum values
+        int row = 0, col = m - 1;
 
         while(row < n && col >= 0) {
             int val = matrix[row][col];
 
             if(val == target) return true;
-            else if(val > target) col--;
-            else row++;
+            else if(val < target) row++;
+            else col--;
         }
 
         return false;
