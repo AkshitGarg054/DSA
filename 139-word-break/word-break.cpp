5992f93d380f1a2cf8f657a87174d435f6fdd196
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-23 15:46:20 +0530

    Time: 19 ms (15.26%) | Memory: 21.7 MB (7.04%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    // this is a very beautiful approach of extending or starting a new substring.
-    // (must remember this approach)
-
-    unordered_set<string> st;
     vector<vector<int>> dp;
+    unordered_set<string> st;
 
-    bool solve(int i, int j, string s) {
+    bool solve(int i, int j, string &s) {
         if(i >= s.size() && j >= s.size()) return true;
-        if(j > s.size()) return false;
+        if(j >= s.size()) return false;
         if(dp[i][j] != -1) return dp[i][j];
 
-        string curr = s.substr(i, j - i + 1);
+        string sub = s.substr(i, j - i + 1);
 
-        if(st.count(curr)) {
-            if(solve(j + 1, j + 1, s)) return dp[i][j] = true; // start new substring
-            if(solve(i, j + 1, s)) return dp[i][j] = true; // extend current
+        if(st.count(sub)) {
+            if(solve(j + 1, j + 1, s)) return dp[i][j] = true; // start new
+            if(solve(i, j + 1, s)) return dp[i][j] = true; // extend
         }
         else {
             if(solve(i, j + 1, s)) return dp[i][j] = true; // extend
         }
-
+        
         return dp[i][j] = false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
         for(auto &s: wordDict) st.insert(s);
-        if(st.count(s)) return true;
 
         dp.assign(n + 1, vector<int>(n + 1, -1));
-        if(solve(0, 0, s)) return true;
-        return false;   
+        return solve(0, 0, s);
     }
 };
