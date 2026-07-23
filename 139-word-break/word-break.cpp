0038f3064e445dabe25681578e170ae4e8d3565e
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-23 15:53:54 +0530

    Time: 15 ms (28.19%) | Memory: 17 MB (27.79%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    vector<int> dp;
     unordered_set<string> st;
 
-    bool solve(int i, int j, string &s) {
-        if(i >= s.size() && j >= s.size()) return true;
-        if(j >= s.size()) return false;
-        if(dp[i][j] != -1) return dp[i][j];
+    bool solve(int i, string &s) {
+        if(i == s.size()) return true;
+        if(dp[i] != -1) return dp[i];
 
-        string sub = s.substr(i, j - i + 1);
+        for(int j = i; j < s.size(); j++) {
+            string sub = s.substr(i, j - i + 1);
+            if(st.count(sub)) {
+                if(solve(j + 1, s)) return dp[i] = true;
+            }
+        }
 
-        if(st.count(sub)) {
-            if(solve(j + 1, j + 1, s)) return dp[i][j] = true; // start new
-            if(solve(i, j + 1, s)) return dp[i][j] = true; // extend
-        }
-        else {
-            if(solve(i, j + 1, s)) return dp[i][j] = true; // extend
-        }
-        
-        return dp[i][j] = false;
+        return dp[i] = false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
         for(auto &s: wordDict) st.insert(s);
 
-        dp.assign(n + 1, vector<int>(n + 1, -1));
-        return solve(0, 0, s);
+        dp.assign(n + 1, -1); 
+        return solve(0, s);
     }
 };
