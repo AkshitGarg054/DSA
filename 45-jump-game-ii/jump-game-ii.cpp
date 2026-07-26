6985cefb32a8241652bd98656ee0656a19bf471d
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-26 15:24:17 +0530

    Time: 1 ms (30.09%) | Memory: 20.7 MB (28.97%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -5,30 +5,19 @@ public:
         if(n == 1) return 0;
 
         int jumps = 0;
-        
-        int i = 0; 
-        while(i < n) {
-            int limit = i + nums[i];
-            if(limit >= n-1) return jumps + 1;
+        int l = 0, r = 0;
 
+        while(r < n - 1) {
+    
             int maxi = INT_MIN;
-            int index = -1;
-
-            int j = i + 1;
-            while(j <= limit) {
-                int new_limit = j + nums[j];
-                if(new_limit > maxi) {
-                    maxi = new_limit;
-                    index = j;
-                }
-                j++;
+            for(int i = l; i <= r; i++) {
+                maxi = max(maxi, i + nums[i]);
             }
-
+            l = r + 1;
+            r = maxi;
             jumps++;
-            if(maxi >= n-1) return jumps + 1; // +1 for last jump
-            i = index;
-        }
+        }        
 
-        return jumps + 1;
+        return jumps;
     }
 };
