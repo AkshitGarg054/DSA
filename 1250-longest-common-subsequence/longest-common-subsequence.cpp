5cf0e614e727d60d7c703bf42a66b500442fee97
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-26 20:15:17 +0530

    Time: 56 ms (5.11%) | Memory: 13.1 MB (83.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
+    int n, m;
+    int dp[1001][1001];
+
+    int solve(int i, int j, string &s1, string &s2) {
+        if(i == n || j == m) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int match = 0, not_match = 0;
+        if(s1[i] == s2[j]) match = 1 + solve(i + 1, j + 1, s1, s2);
+        not_match = 0 + max(solve(i, j + 1, s1, s2), solve(i + 1, j, s1, s2));
+
+        return dp[i][j] = max(match, not_match);
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
+        n = text1.size();
+        m = text2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(text1[i] == text2[j]) dp[i + 1][j + 1] = 1 + dp[i][j];
-                else dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
-            }
-        }
-
-        return dp[n][m];
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, text1, text2);
     }
 };
