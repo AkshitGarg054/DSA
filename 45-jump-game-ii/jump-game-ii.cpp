d8bff936923df14e3d7ccf9907468ec220d7ca72
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-26 15:20:08 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (28.97%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,20 +3,21 @@ public:
     int jump(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return 0;
-        int mini = INT_MAX;
-        int jumps = 0;
 
-        int i = 0;
+        int jumps = 0;
+        
+        int i = 0; 
         while(i < n) {
             int limit = i + nums[i];
-            if(limit >= n - 1) return jumps + 1;
+            if(limit >= n-1) return jumps + 1;
+
             int maxi = INT_MIN;
+            int index = -1;
 
             int j = i + 1;
-            int index = -1;
             while(j <= limit) {
                 int new_limit = j + nums[j];
-                if(new_limit >= maxi) {
+                if(new_limit > maxi) {
                     maxi = new_limit;
                     index = j;
                 }
@@ -24,7 +25,7 @@ public:
             }
 
             jumps++;
-            if(maxi >= n - 1) return jumps + 1; // 1 added for last jump
+            if(maxi >= n-1) return jumps + 1; // +1 for last jump
             i = index;
         }
 
