202d0a39d99d5e4c272870cad60fbed682343f1b
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-26 20:42:46 +0530

    Time: 35 ms (11.40%) | Memory: 27.5 MB (95.29%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<int>> dp;
+    int dp[301][301];
 
-    int solve(int i, int j, vector<vector<char>> &matrix) {
-        if(i < 0 || j < 0) return 0;
+    int solve(int i, int j, vector<vector<char>> &grid) {
+        if(grid[i][j] == '0') return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        int left = solve(i, j - 1, matrix);
-        int up = solve(i - 1, j, matrix);
-        int diag = solve(i - 1, j - 1, matrix);
+        int ans = 1; // the cell itself
+        if(i > 0 && j > 0 && grid[i - 1][j] == '1' && grid[i][j - 1] == '1' && grid[i - 1][j - 1] == '1') {
+            ans = 1 + min({solve(i - 1, j, grid), solve(i, j - 1, grid), solve(i - 1, j - 1, grid)});
+        }
 
-        return dp[i][j] = 1 + min({left, up, diag});
+        return dp[i][j] = ans;
     }
 
     int maximalSquare(vector<vector<char>>& matrix) {
         n = matrix.size();
         m = matrix[0].size();
 
-        dp.assign(n, vector<int>(m, -1));
-        int side = 0;
+        memset(dp, -1, sizeof(dp));
 
+        int maxi = INT_MIN;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(matrix[i][j] == '1') side = max(side, solve(i, j, matrix));
-                else dp[i][j] = 0;
+                if(matrix[i][j] == '1') {
+                    int len = solve(i, j, matrix);
+                    maxi = max(maxi, len);
+                }
             }
         }
 
-        return side * side;
+        return 1LL * maxi * maxi;
     }
 };
