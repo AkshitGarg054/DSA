5694ee47ed76cefaa76d46d1f8225c4a697835c0
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-27 16:14:41 +0530

    Time: 50 ms (11.39%) | Memory: 11.6 MB (85.95%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
+    // s1 = "ac" , s2 = "abc"
+    // At i = 1, j = 1, we need to insert b ---> s1 = "abc"
+    // By doing this, i remains at 'c' and j incremented.
+
     int n, m;
-    vector<vector<int>> dp;
+    int dp[501][501];
 
-    int solve(int i, int j, string &word1, string &word2) {
-        if(i == n) return m - j; // this much extra operations are needed
-        if(j == m) return n - i;
+    int solve(int i, int j, string &s1, string &s2) {
+        if(i >= s1.size() && j >= s2.size()) return 0;
+        if(i == s1.size()) return s2.size() - j; // insert operations.
+        if(j == s2.size()) return s1.size() - i; // delete operations.
+
         if(dp[i][j] != -1) return dp[i][j];
+        int ans = 1e9;
 
-        int match = INT_MAX;
-        if(word1[i] == word2[j]) match = 0 + solve(i + 1, j + 1, word1, word2);
+        if(s1[i] == s2[j]) ans = min(ans, solve(i + 1, j + 1, s1, s2));
+        else {
+            int insert = 1 + solve(i, j + 1, s1, s2);
+            int del = 1 + solve(i + 1, j, s1, s2);
+            int replace = 1 + solve(i + 1, j + 1, s1, s2);
+            ans = min({ans, insert, del, replace});
+        }
 
-        int insert = 1 + solve(i, j + 1, word1, word2);
-        int del = 1 + solve(i + 1, j, word1, word2);
-        int replace = 1 + solve(i + 1, j + 1, word1, word2);
-
-        return dp[i][j] = min({match, insert, del, replace});
+        return dp[i][j] = ans;
     }
 
     int minDistance(string word1, string word2) {
         n = word1.size();
         m = word2.size();
 
-        dp.resize(n, vector<int>(m, -1));
+        memset(dp, -1, sizeof(dp));
         return solve(0, 0, word1, word2);
     }
 };
