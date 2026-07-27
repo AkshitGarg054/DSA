6904447863dceb1d42f571692f97b1aa52299373
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-27 13:35:54 +0530

    Time: 5 ms (66.07%) | Memory: 28.9 MB (80.47%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<int>> dp;
+    int dp[301][301];
 
-    int solve(int i, int j, vector<vector<int>>& matrix) {
-        if(i < 0 || j < 0) return 0;
+    int solve(int i, int j, vector<vector<int>> &grid) {
+        if(grid[i][j] == 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        int left = solve(i, j - 1, matrix);
-        int up = solve(i - 1, j, matrix);
-        int diag = solve(i - 1, j - 1, matrix);
+        int ans = 1; // the cell itself 
+        if(i > 0 && j > 0 && grid[i - 1][j] == 1 && grid[i][j - 1] == 1 && grid[i - 1][j - 1] == 1) {
+            ans = 1 + min({solve(i - 1, j, grid), solve(i, j - 1, grid), solve(i - 1, j - 1, grid)});
+        }
 
-        return dp[i][j] = 1 + min({left, up, diag});
+        return dp[i][j] = ans;
     }
 
     int countSquares(vector<vector<int>>& matrix) {
         n = matrix.size();
         m = matrix[0].size();
+        int count = 0;
 
-        dp.assign(n, vector<int>(m, -1));
+        memset(dp, -1, sizeof(dp));
 
-        int total = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(matrix[i][j] == 1) total += solve(i, j, matrix);
-                else dp[i][j] = 0;
+                if(matrix[i][j] == 1) count += solve(i, j, matrix); // side = number of square submatrices
             }
         }
 
-        return total;
+        return count;
     }
 };
