404aaeed951737bf4d0c07ab6015b36cb83381d7
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-30 13:03:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (99.45%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -2,36 +2,17 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int n = nums.size();
+        int i = 0; // we will keep incrementing i and it will stop at the ele equal to 'val' for swapping
+        int count = 0;
 
-        int i = -1, j = -1; // i -> first 'val' element, j -> first non-val ele after 'val'
-        for(int index = 0; index < n; index++) {
-            if(nums[index] == val) {
-                i = index;
-                break;
+        for(int j = 0; j < n; j++) {
+            if(nums[j] != val) {
+                swap(nums[i], nums[j]);
+                i++;
+                count++;
             }
         }
 
-        if(i == -1) return n;
-
-        for(int index = i + 1; index < n; index++) {
-            if(nums[index] != val) {
-                j = index;
-                break;
-            }
-        }
-
-        if(j == -1) return i;
-
-        while(j < n) {
-            if(nums[j] == val) j++;
-            else swap(nums[i++], nums[j++]);
-        }
-
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] != val) ans++;
-        }
-
-        return ans;
+        return count;
     }
 };
