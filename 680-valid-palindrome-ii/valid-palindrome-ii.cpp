ba42754e7faad07484d04caab189ae47e26c813d
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-30 13:48:01 +0530

    Time: 6 ms (33.79%) | Memory: 22.2 MB (42.02%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool check(string &s, int l, int r) {
+        while(l <= r) {
+            if(s[l] != s[r]) return false;
+            l++, r--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int n = s.size();
+        int l = 0, r = n - 1;
+
+        while(l <= r) {
+            if(s[l] == s[r]) l++, r--;
+            else return check(s, l, r - 1) || check(s, l + 1, r);
+        }
+
+        return true; // if else condition didn't run, then s is already a palindrome
+    }
+};
