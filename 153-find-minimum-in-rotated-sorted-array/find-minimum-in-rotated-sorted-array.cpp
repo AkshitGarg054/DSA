f874c4c45fe4b10e8bd8d96cf7719d73fd8e7c2e
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-30 17:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (50.89%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    // hme minimum dhundna haiii aurr hme pta haii ki rotated sorted array me ek side hmesha sorted rehti haii
-    // and we know ki sorted array me se minimum dhundna to easy haii
-    // so sorted part me se minimum lete jao aur array ko shrink krte jaoo.
-    // at the end, hme minimum mil chuka hoga.
-
     int findMin(vector<int>& nums) {
         int n = nums.size();
-
         int low = 0, high = n - 1;
-        int ans = INT_MAX;
+        int mini = INT_MAX;
 
         while(low <= high) {
             int mid = low + (high - low) / 2;
 
-            if(nums[low] <= nums[mid]) { // check if sorted
-                ans = min(ans, nums[low]);
-                low = mid + 1; // elimiate this sorted half
+            if(nums[low] <= nums[mid]) {
+                mini = min(mini, nums[low]);
+                low = mid + 1;
             }
             else {
-                ans = min(ans, nums[mid]);
+                mini = min(mini, nums[mid]);
                 high = mid - 1;
             }
         }
 
-        return ans;
+        return mini;
     }
 };
