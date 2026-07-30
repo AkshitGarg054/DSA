4412f23f58a763f3ec38c514b0be3f1d12dc8b2e
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-30 20:54:39 +0530

    Time: 155 ms (7.31%) | Memory: 91.4 MB (8.14%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,34 +4,29 @@ public:
         int n = nums.size();
         if(n == 0) return 0;
 
-        unordered_map<int, bool> mp;
-        for(auto ele : nums) mp[ele] = false; // store all elements in a map
+        map<int, bool> mp;
+        for(auto &ele: nums) mp[ele] = true;
+        
+        auto it = mp.begin();
+        auto [val, found] = *it;
 
+        int prev = val;
         int maxi = 1;
+        int len = 1;
 
-        for(int i = 0; i < n; i++) {
-            int ele = nums[i];
-            if(mp[ele] == true) continue;
+        it++;
+        while(it != mp.end()) {
+            auto [val, found] = *it;
 
-            mp[ele] = true;
-            int count = 1;
-
-            int temp = ele;
-            while(mp.count(temp - 1) && !mp[temp - 1]) {
-                count++;
-                mp[temp - 1] = true;
-                temp--;
+            if(val == prev + 1) {
+                len++;
+                maxi = max(maxi, len);
             }
+            else len = 1;
 
-            temp = ele;
-            while(mp.count(temp + 1) && !mp[temp + 1]) {
-                count++;
-                mp[temp + 1] = true;
-                temp++;
-            }
-
-            maxi = max(maxi, count);
-        } 
+            prev = val;
+            it++;
+        }
 
         return maxi;
     }
