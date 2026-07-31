a394da8ca1c6fb2c3428afccf6203c25357bb593
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-07-31 16:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.54%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    // inplace ke liye hme pointers hi rakhne hote hn.
-    // So, we need three pointers here.
+    // Simply think about where each number should go: 
+    // 0 should go at the front, 2 at the back and 1 can stay in the middle.
+    // So, let's maintain:
+    // left = where the next 0 should be placed.
+    // right = where the next 2 should be placed.
+    // i = current element we're checking
 
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0, mid = 0;
-        int high = n - 1;
+        int left = 0;
+        int i = 0;
+        int right = nums.size() - 1;
 
-        while(mid <= high) {
-            if(nums[mid] == 0) {
-                swap(nums[low], nums[mid]);
-                low++, mid++;
+        while(i <= right) {
+            if(nums[i] == 0) {
+                swap(nums[i], nums[left]);
+                left++;
+                i++; // increment both bcz the element we swapped from left has already been processed
             }
-            else if(nums[mid] == 1) mid++;
+            else if(nums[i] == 1) i++;
             else {
-                swap(nums[mid], nums[high]);
-                high--;
+                swap(nums[i], nums[right]);
+                right--;
             }
-        } 
+        }
     }
 };
