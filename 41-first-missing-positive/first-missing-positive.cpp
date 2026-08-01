7fa03312ea7aec3ee4ef8530aad98adeac418760
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-08-01 20:26:10 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (86.40%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    // If a value x exists, then it belongs at index (x - 1).
-    // So we repeatedly swap until every valid value reaches its correct index.
-
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        
-        // with each swap, one element goes to its correct position, so the approach is O(n)
+
+        // we need to set all positive elements at their correct position. (For nums[i], correct pos is nums[i] - 1) ...see that there is no correct position for 0.
+        // After this, the first index which does not contain its correct element, will contain our answer.
         for(int i = 0; i < n; i++) {
-            while(nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+            while(nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) { // check whether nums[i] is present at its correct position (nums[i] - 1) or not.
                 swap(nums[i], nums[nums[i] - 1]);
             }
         }
