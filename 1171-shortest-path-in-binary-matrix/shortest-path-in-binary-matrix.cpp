6202ddd464fc93b3b7e6be7b0d4d673e0cf6e493
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-08-02 18:04:04 +0530

    Time: 16 ms (64.25%) | Memory: 25.5 MB (48.84%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    vector<vector<int>> dirs = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+    vector<vector<int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, -1}, {-1, 1}, {1, 1}, {-1, -1}};
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
+        if(grid[0][0] == 1) return -1;
+        if(grid[n - 1][m - 1] == 1) return -1;
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vis[0][0] = 1;
 
         queue<pair<int, int>> q;
         q.push({0, 0});
-        grid[0][0] = 1; // mark source visited
 
-        int level = 1;
+        int count = 1;
 
         while(!q.empty()) {
             int sz = q.size();
+            bool found = false;
 
             while(sz--) {
                 auto [r, c] = q.front();
                 q.pop();
 
-                if(r == n - 1 && c == m - 1) return level;
+                if(r == n - 1 && c == m - 1) return count;
 
                 for(auto &d: dirs) {
                     int nr = r + d[0];
                     int nc = c + d[1];
 
                     if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-                    if(grid[nr][nc] == 0) {
-                        grid[nr][nc] = 1; // mark visited
-                        q.push({nr, nc});
-                    }
+                    if(grid[nr][nc] == 1) continue;
+                    if(vis[nr][nc]) continue;
+
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                    found = true;
                 }
             }
 
-            level++;
+            if(found) count++;
         }
 
         return -1;
