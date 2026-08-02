d2f8917ef9eef38214b3d8ee943786e2add90a42
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-08-02 13:52:40 +0530

    Time: 37 ms (38.81%) | Memory: 17.1 MB (71.54%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,37 +1,64 @@
 class Solution {
 public:
+    // An alternating string can have two possibilites: 010101... or 101010....
+    // So, if there were no rotation, then answer = min(mismatches with pattern1, mismatches with pattern2);
+    // Now, trying every rotating any calculate the minimum mismatches is difficult.
+    // So, we create t = s + s.
+    // Now, every rotation of s appears as a substring of length n inside t.
+    // For every window of size n in t, calculate the min operations and take the min of all.
+
+    int solve1(string &s, int l, int r) {
+        // To check with 01010...
+        int ops = 0;
+        char expected = '0';
+        for(int i = l; i <= r; i++) {
+            if(s[i] != expected) ops++;
+            expected = (expected == '0' ? '1' : '0');
+        }
+        return ops;
+    }
+
+    int solve2(string &s, int l, int r) {
+        // To check with 10101...
+        int ops = 0;
+        char expected = '1';
+        for(int i = l; i <= r; i++) {
+            if(s[i] != expected) ops++;
+            expected = (expected == '0' ? '1' : '0');
+        }
+        return ops;
+    }
+
     int minFlips(string s) {
         int n = s.size();
         string t = s + s;
 
-        string alt1 = "", alt2 = "";
-        
-        for(int i = 0; i < 2*n; i++) {
-            alt1 += (i % 2 == 0 ? '0' : '1'); 
-            alt2 += (i % 2 == 0 ? '1' : '0');
-        }
+        int diff1 = solve1(t, 0, n - 1);
+        int diff2 = solve2(t, 0, n - 1);
 
-        int diff1 = 0, diff2 = 0;
-        int ans = INT_MAX;
+        int mini = min(diff1, diff2);
+        int l = 0, r = n;
 
-        // sliding window
-        int left = 0;
-        for(int right = 0; right < 2*n; right++) {
-            if(t[right] != alt1[right]) diff1++;
-            else diff2++;
+        while(r < 2*n) {
+            // incoming ele...
+            char exp1 = (r % 2 == 0) ? '0' : '1'; // 01010 contains 0 at even indices
+            char exp2 = (r % 2 == 0) ? '1' : '0'; // 10101 contains 0 at odd indices
 
-            // keep window size = n
-            if(right - left + 1 > n) {
-                if(t[left] != alt1[left]) diff1--;
-                else diff2--;
-                left++;
+            if(t[r] != exp1) diff1++;
+            if(t[r] != exp2) diff2++;
+
+            while(r - l + 1 > n) {
+                char exp1 = (l % 2 == 0) ? '0' : '1'; 
+                char exp2 = (l % 2 == 0) ? '1' : '0'; 
+                if(t[l] != exp1) diff1--;
+                if(t[l] != exp2) diff2--;
+                l++;
             }
 
-            if(right - left + 1 == n) {
-                ans = min({ans, diff1, diff2});
-            }
+            mini = min({mini, diff1, diff2});
+            r++;
         }
 
-        return ans;
+        return mini;
     }
 };
