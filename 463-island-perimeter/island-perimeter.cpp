00756cd78bf0bfb65aab5a4e37fced46221da8e7
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-08-02 18:18:51 +0530

    Time: 5 ms (48.40%) | Memory: 102.2 MB (44.09%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    // For each cell, we need to check ki uske neighbors pe land hai ya water.
-    // As, each land cell can contribute either 0, 1, 2, 3 or 4 in the perimeter,
-    // so we can simply traverse in the grid, and jb bhi land cell aaye to we can check ki uske 4 neighbors pe kya kya haii and accordingly increment the parameter.
-    // so both the submitted approaches
-
-    int n, m;
-
-    int dfs(vector<vector<int>>& grid, int i, int j) {
-        if(i < 0 || j < 0 || i >= n || j >= m) return 1;
-        if(grid[i][j] == 0) return 1;
-        if(grid[i][j] == -1) return 0; // if already visited
-
-        grid[i][j] = -1;
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
 
         int perimeter = 0;
-
-        perimeter += dfs(grid, i+1, j);
-        perimeter += dfs(grid, i-1, j);
-        perimeter += dfs(grid, i, j+1);
-        perimeter += dfs(grid, i, j-1);
-
-        return perimeter;
-    }
-
-    int islandPerimeter(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 1) {
-                    return dfs(grid, i, j);
+                    int count = 0;
+
+                    if(i == 0 || grid[i - 1][j] == 0) count++;
+                    if(i + 1 == n || grid[i + 1][j] == 0) count++;
+                    if(j + 1 == m || grid[i][j + 1] == 0) count++;
+                    if(j == 0 || grid[i][j - 1] == 0) count++;
+
+                    perimeter += count;
                 }
             }
         }
 
-        return 0;
+        return perimeter;
     }
 };
