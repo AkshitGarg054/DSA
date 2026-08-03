8e9d861ab4368646e99e25b52b5ae2ec3384c9b9
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-08-03 13:01:04 +0530

    Time: 3 ms (86.29%) | Memory: 23.8 MB (35.82%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    // REVERSING THE DIRECTION trick.
-    // Instead of going from cell --> ocean, go from ocean --> cell.
     int n, m;
     vector<vector<int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-    void dfs(int r, int c, vector<vector<int>>& heights, vector<vector<bool>>& vis) {
-        vis[r][c] = true;
+    void dfs1(int r, int c, vector<vector<int>> &pacific, vector<vector<int>> &grid) {
+        pacific[r][c] = 1;
 
         for(auto &d: dirs) {
             int nr = r + d[0];
             int nc = c + d[1];
 
-            if(nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
-            if(vis[nr][nc]) continue;
+            if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+            if(pacific[nr][nc] == 1) continue;
+            if(grid[nr][nc] >= grid[r][c]) dfs1(nr, nc, pacific, grid);
+        }
+    }
 
-            if(heights[nr][nc] >= heights[r][c]) { // we reversed the direction
-                dfs(nr, nc, heights, vis);
-            }
+    void dfs2(int r, int c, vector<vector<int>> &atlantic, vector<vector<int>> &grid) {
+        atlantic[r][c] = 1;
+
+        for(auto &d: dirs) {
+            int nr = r + d[0];
+            int nc = c + d[1];
+
+            if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+            if(atlantic[nr][nc] == 1) continue;
+            if(grid[nr][nc] >= grid[r][c]) dfs2(nr, nc, atlantic, grid);
         }
     }
 
@@ -25,20 +33,20 @@ public:
         n = heights.size();
         m = heights[0].size();
 
-        vector<vector<bool>> pacific(n, vector<bool>(m, false));
-        vector<vector<bool>> atlantic(n, vector<bool>(m, false));
+        vector<vector<int>> pacific(n, vector<int>(m, 0));
+        vector<vector<int>> atlantic(n, vector<int>(m ,0));
 
-        for(int i = 0; i < n; i++) dfs(i, 0, heights, pacific);
-        for(int j = 0; j < m; j++) dfs(0, j, heights, pacific);
+        for(int i = 0; i < n; i++) dfs1(i, 0, pacific, heights);
+        for(int j = 0; j < m; j++) dfs1(0, j, pacific, heights);
 
-        for(int i = 0; i < n; i++) dfs(i, m - 1, heights, atlantic);
-        for(int j = 0; j < m; j++) dfs(n - 1, j, heights, atlantic);
+        for(int i = 0; i < n; i++) dfs2(i, m - 1, atlantic, heights);
+        for(int j = 0; j < m; j++) dfs2(n - 1, j, atlantic, heights);
 
         vector<vector<int>> ans;
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(pacific[i][j] && atlantic[i][j]) ans.push_back({i, j});
+                if(pacific[i][j] == 1 && atlantic[i][j] == 1) ans.push_back({i, j});
             }
         }
 
