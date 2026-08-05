ad6b035cd5533c88588e29b940118e0c555db7d3
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-08-05 22:47:10 +0530

    Time: 16 ms (58.71%) | Memory: 25.7 MB (35.39%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
-    // for every 0 cell, calculate the distance to its nearest 1
-    // and then take the maximum of all distances.
-    // instead of running BFS from all 0s one by one
-    // run BFS from all 1s together, the last zero reached will be our answer
-
     vector<vector<int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     int maxDistance(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m = grid[0].size();
 
+        vector<vector<int>> dist(n, vector<int>(n, -1));
         queue<pair<int, int>> q;
+
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1) q.push({i, j});
-            }
-        }
-
-        int steps = 0;
-
-        while(!q.empty()) {
-            int sz = q.size();
-            bool added = false;
-
-            while(sz--) {
-                auto [r, c] = q.front();
-                q.pop();
-
-                for(auto &d: dirs) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-
-                    if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-                    if(grid[nr][nc] == 0) {
-                        grid[nr][nc] = 1;
-                        q.push({nr, nc});
-                        added = true;
-                    }
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    q.push({i, j});
+                    dist[i][j] = 0;
                 }
             }
-
-            if(added) steps++;
         }
-        
-        if(steps == 0) return -1;
-        return steps;
+
+        while(!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(auto &d: dirs) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
+                if(dist[nr][nc] != -1) continue;
+
+                if(grid[nr][nc] == 0) {
+                    dist[nr][nc] = 1 + dist[r][c];
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        int maxi = -1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                maxi = max(maxi, dist[i][j]);
+            }
+        }
+
+        if(maxi == 0) return -1;
+        return maxi;
     }
 };
