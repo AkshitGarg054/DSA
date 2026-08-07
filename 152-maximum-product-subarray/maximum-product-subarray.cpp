8c8474d6d70cb561e37340ba279a449a7cf6e613
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-08-07 21:11:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.05%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    // this approach is very similar to kadane.
-
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        int product = 1, maxi = INT_MIN;
+
+        int maxi = INT_MIN;
+        int product = 1;
 
         // left to right pass
         for(int i = 0; i < n; i++) {
@@ -13,12 +13,13 @@ public:
             if(product == 0) product = 1;
         }
 
-        // right to left pass
         product = 1;
+
+        // right to left pass
         for(int i = n - 1; i >= 0; i--) {
             product *= nums[i];
             maxi = max(maxi, product);
-            if(product == 0) product = 1; 
+            if(product == 0) product = 1;
         }
 
         return maxi;
