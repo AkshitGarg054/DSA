7752b8f76dc0a05f59fa2d49efc396e777622ead
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-09-12 23:06:50 +0530

    Time: 488 ms (72.84%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,7 +1,3 @@
-# (ON w1.id = w2.id) is  not used because it joins the same row with itself, so So DATEDIFF(w1.recordDate, w2.recordDate) will always be 0
-# We need to compare today’s row with yesterday’s row, so the join should be based on date difference, not id.
-
-SELECT w1.id
+SELECT w2.id as id
 FROM Weather w1 JOIN Weather w2
-ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+ON DATEDIFF(w2.recordDate, w1.recordDate) = 1 AND w2.temperature > w1.temperature
