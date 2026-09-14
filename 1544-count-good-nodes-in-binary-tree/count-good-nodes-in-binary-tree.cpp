c311ce0074f71fe445b52b617a562bd186e27828
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-09-14 20:12:08 +0530

    Time: 90 ms (83.64%) | Memory: 88.4 MB (30.27%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int count = 0;
 
+    // as maxi is passed by value, so each recursion call will get its own copy of maxi. So, maximum from left subtree won't interfere in the right tree.
     void solve(TreeNode* root, int maxi) {
         if(root == NULL) return;
 
