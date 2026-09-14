132d128f58bdba8be820cc12f72f1eb41a81001f
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-09-14 20:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (47.28%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    unordered_map<TreeNode*, TreeNode*> mp; // mp[child] = parent
-
-    void findParent(TreeNode* root, TreeNode* parent) {
-        if(root == NULL) return;
-
-        mp[root] = parent;
-        findParent(root -> left, root);
-        findParent(root -> right, root);
-    }
-
+    // We are checking if the subtree contains any 1 or not.
     int solve(TreeNode* root) {
         if(root == NULL) return 0;
 
         int left = solve(root -> left);
         int right = solve(root -> right);
 
-        if(left == 0 && right == 0 && root -> val == 0) {
-            if(mp[root] -> left == root) mp[root] -> left = NULL;
-            if(mp[root] -> right == root) mp[root] -> right = NULL;
-        }
+        if(left == 0) root -> left = 0;
+        if(right == 0) root -> right = 0;
 
-        if(root == NULL) return 0;
-        return left + right + (root -> val);
+        return left | right | root -> val;
     }
 
     TreeNode* pruneTree(TreeNode* root) {
         if(root == NULL) return NULL;
-
-        TreeNode* temp = new TreeNode(-1);
-        findParent(root, temp);
 
         solve(root);
         if(root -> val == 0 && root -> left == NULL && root -> right == NULL) return NULL;
