b1fd049b988bc3b7b685e891c9c2b834cc1fb2d5
AkshitGarg054 <137501798+AkshitGarg054@users.noreply.github.com>
2026-09-14 17:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (55.00%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -3,18 +3,19 @@ public:
     int solve(TreeNode* root) {
         if(root == NULL) return 0;
 
-        int left = 1 + solve(root -> left);
-        int right = 1 + solve(root -> right);
+        int left = solve(root -> left);
+        int right = solve(root -> right);
 
-        if(abs(left - right) <= 1) return max(left, right); // return the height upwards towards the root
-        return 1e9; // return this poison value upwards
+        if(abs(left - right) <= 1) return max(left, right) + 1;
+        return 1e9;
     }
 
     bool isBalanced(TreeNode* root) {
         if(root == NULL) return true;
+        if(root -> left == NULL && root -> right == NULL) return true;
 
-        int height = solve(root);
-        if(height >= 1e9) return false;
+        int ans = solve(root);
+        if(ans >= 1e9) return false;
         return true;
     }
 };
